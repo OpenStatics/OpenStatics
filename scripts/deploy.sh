98239c3e38ac7d4d0cac7386124f1503211b4c7f
Zichao <zichao2018@gmail.com>
2019-10-08 09:13:50 -0400

    update scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# dummy deploy
+set -e 
+
+function buildProd {
+    npm run build
+}
+
+buildProd
+
+git config --global user.name "Circle CI"
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
+
+REPO="https://github.com/OpenStatics/openstatics.github.io"
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+

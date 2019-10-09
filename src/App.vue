88eb3910e6988fca0906719998e58dbeea2daf85
Zichao <zichao2018@gmail.com>
2019-10-09 17:55:58 -0400

    redesign homepage structure and add first vector project

--- src/App.vue
+++ src/App.vue
@@ -17,35 +17,57 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
           </li>
           <li v-for="(items, category) in pages" :key="category" class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              {{ category }}
-            </a>
-            <div class="dropdown-menu" v-for="item in items" :key="item.name" :title="item.description">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >{{ category }}</a>
+            <div
+              class="dropdown-menu"
+              v-for="item in items"
+              :key="item.name"
+              :title="item.description"
+            >
               <a class="dropdown-item" @click="current = item.name">{{ item.title }}</a>
             </div>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
       </div>
     </nav>
-    <div class="row justify-content-center">
+    <div class='mx-2 my-2'>
+      <template v-if="current">
+        <component :is="current"></component>
+      </template>
+      <template v-else>Home page</template>
+    </div>
+    <!-- <div class="row justify-content-center">
       <div class="col">&nbsp;</div>
       <div class="col col-10">
         <template v-if="current">
           <component :is="current"></component>
         </template>
-        <template v-else>
-          Home page
-        </template>
+        <template v-else>Home page</template>
       </div>
       <div class="col">&nbsp;</div>
-    </div>
+    </div> -->
   </div>
 </template>
 
--- src/components/pages/vectors/vector.vue
+++ src/components/pages/vectors/vector.vue
@@ -1,65 +1,136 @@
 <template>
   <div>
-    <div id="box1" class="jsx-graph my-2"></div>
-    <div id="box2" class="jsx-graph my-2"></div>
+    <div id="box1" class="jsx-graph2 my-2"></div>
+    <div id="box2" class="jsx-graph my-2 text-center"></div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      board: null,
+      lol: 10
+    };
+  },
   mounted() {
-    var b1 = JXG.JSXGraph.initBoard("box1", { boundingbox: [-7.5, 7.5, 12.5, -7.5], axis: true });
-    b1.suspendUpdate();
-
-    var a = b1.create("slider", [[-5, -2], [5, -2], [-5, 1, 5]], { name: "a" });
-    var b = b1.create("slider", [[-5, -3], [5, -3], [-5, 0, 5]], { name: "b" });
-    var c = b1.create("slider", [[-5, -4], [5, -4], [-5, 0, 5]], { name: "c" });
-    var d = b1.create("slider", [[-5, -5], [5, -5], [-5, 1, 5]], { name: "d" });
-
-    var v = b1.create("point", [2, 2], { size: 3, name: "v" });
-    var w = b1.create("point", [-2, 1], { size: 3, name: "w" });
-    var va = b1.create("line", [[0, 0], v], { straightFirst: false, straightLast: false, lastArrow: true });
-    var wa = b1.create("line", [[0, 0], w], { straightFirst: false, straightLast: false, lastArrow: true });
-    b1.unsuspendUpdate();
-
-    var b2 = JXG.JSXGraph.initBoard("box2", { boundingbox: [-7.5, 7.5, 12.5, -7.5], axis: true });
+    // create two boards: b1 is the control and b2 is the display
+    const b1 = JXG.JSXGraph.initBoard("box1", { boundingbox: [-10, 10, 10, -10] });
+    const b2 = JXG.JSXGraph.initBoard("box2", { boundingbox: [-15, 15, 15, -15], axis: true, keepAspectRatio: true });
     b1.addChild(b2);
-    b2.suspendUpdate();
 
-    var v2 = b2.create(
+    // set slider for force and angle
+    const force = b1.create("slider", [[1, -2], [6, -2], [0, 1, 5]]);
+    const angle = b1.create("slider", [[1, -4], [6, -4], [0, 0, 360]]);
+
+    // create two points for reference
+    const origin_point = b2.create("point", [0, 0], { fixed: true }, { name: "O" });
+    const end_point = b2.create(
       "point",
       [
         function() {
-          return a.Value() * v.X() + b.Value() * v.Y();
+          return Math.cos((angle.Value() / 180) * Math.PI) * force.Value();
         },
         function() {
-          return c.Value() * v.X() + d.Value() * v.Y();
+          return Math.sin((angle.Value() / 180) * Math.PI) * force.Value();
         }
       ],
-      { face: "[]", size: 2, name: "v'" }
+      { name: "v" }
     );
-    var w2 = b2.create(
-      "point",
+
+    // create the main vector
+    const vector = b2.create("line", [origin_point, end_point], { straightFirst: false, straightLast: false, lastArrow: true });
+
+    // projection of F on x
+    var check_prof_F_on_x = b1.create("checkbox", [1, -6, "Projection of F on x"], {});
+    const proj_F_on_x = b2.create(
+      "line",
       [
-        function() {
-          return a.Value() * w.X() + b.Value() * w.Y();
-        },
-        function() {
-          return c.Value() * w.X() + d.Value() * w.Y();
-        }
+        origin_point,
+        [
+          function() {
+            return Math.cos((angle.Value() / 180) * Math.PI) * force.Value();
+          },
+          0
+        ]
       ],
-      { face: "[]", size: 2, name: "w'" }
-    );
-    var va2 = b2.create("line", [[0, 0], v2], { straightFirst: false, straightLast: false, lastArrow: true });
-    var wa2 = b2.create("line", [[0, 0], w2], { straightFirst: false, straightLast: false, lastArrow: true });
-    var t = b2.create("text", [
-      -6,
-      -2,
-      function() {
-        return "|" + a.Value().toFixed(2) + " " + b.Value().toFixed(2) + "|" + "<br>" + "|" + c.Value().toFixed(2) + " " + d.Value().toFixed(2) + "|";
+      {
+        straightFirst: false,
+        straightLast: false,
+        lastArrow: true,
+        visible: function() {
+          return check_prof_F_on_x.Value();
+        }
       }
-    ]);
-    b2.unsuspendUpdate();
+    );
+
+    // Projection of F on y
+    var check_prof_F_on_y = b1.create("checkbox", [1, -8, "Projection of F on y"], {});
+    const proj_F_on_y = b2.create(
+      "line",
+      [
+        origin_point,
+        [
+          0,
+          function() {
+            return Math.sin((angle.Value() / 180) * Math.PI) * force.Value();
+          }
+        ]
+      ],
+      {
+        straightFirst: false,
+        straightLast: false,
+        lastArrow: true,
+        visible: function() {
+          return check_prof_F_on_y.Value();
+        }
+      }
+    );
+
+    // Resolution of F into components
+    var check_res_F = b1.create("checkbox", [1, -10, "Resolution of F into components"], {});
+    const res_F_y = b2.create(
+      "line",
+      [
+        [
+          0,
+          function() {
+            return Math.sin((angle.Value() / 180) * Math.PI) * force.Value();
+          }
+        ],
+        end_point
+      ],
+      {
+        straightFirst: false,
+        straightLast: false,
+        lastArrow: true,
+        visible: function() {
+          return check_res_F.Value();
+        },
+        dash: 2
+      }
+    );
+    const res_F_x = b2.create(
+      "line",
+      [
+        [
+          function() {
+            return Math.cos((angle.Value() / 180) * Math.PI) * force.Value();
+          },
+          0
+        ],
+        end_point
+      ],
+      {
+        straightFirst: false,
+        straightLast: false,
+        lastArrow: true,
+        visible: function() {
+          return check_res_F.Value();
+        },
+        dash: 2
+      }
+    );
   }
 };
 export const meta = {
@@ -70,7 +141,11 @@ export const meta = {
 
 <style>
 .jsx-graph {
-  width: 100%;
-  height: 45vh;
+  width: 800px !important;
+  height: 800px !important;
+}
+.jsx-graph2 {
+  width: 100% !important;
+  height: 30vh !important;
 }
 </style>

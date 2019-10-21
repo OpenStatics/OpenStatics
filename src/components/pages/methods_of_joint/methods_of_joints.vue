f3a65bf9eda1e587d093e909c16a3022b0fbc54d
Zichao <zichao2018@gmail.com>
2019-10-20 21:36:41 -0400

    update free body diagram

--- src/components/pages/methods_of_joint/methods_of_joints.vue
+++ src/components/pages/methods_of_joint/methods_of_joints.vue
@@ -1,65 +1,82 @@
 <template>
   <div>
-    <div id="box1" class="jsx-graph2 my-2"></div>
     <div id="box2" class="jsx-graph my-2 text-center"></div>
+    <button>Analysis on A</button>
+    <button>Analysis on B</button>
+    <button>Analysis on C</button>
+    <button>Analysis on D</button>
+    <button>Analysis on E</button>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+  },
   mounted() {
-    const b1 = JXG.JSXGraph.initBoard("box1", { boundingbox: [-10, 10, 10, -10] });
-    const b2 = JXG.JSXGraph.initBoard("box2", { boundingbox: [-15, 15, 15, -15], keepAspectRatio: true });
-    b1.addChild(b2);
+    const fixedDecimal = 4;
 
-    const force = b1.create("slider", [[-3, 0], [3, 0], [1, 1, 2]], { name: "Load F_C" });
-    const angle = b1.create("slider", [[-3, -2], [3, -2], [0, 90, 180]], { name: "Angle(&Phi;)" });
+    const b2 = JXG.JSXGraph.initBoard("box2", { boundingbox: [-15, 15, 15, -15], keepAspectRatio: true, showCopyright: false });
+
+    const force = b2.create("slider", [[-3, -10], [3, -10], [1, 1, 2]], { name: "Load F_C" });
+    const angle = b2.create("slider", [[-3, -12], [3, -12], [0, 90, 180]], { name: "Angle(&Phi;)" });
 
     // get RA_x value
+    const getR_Ax = function() {
+      const R_Ax = Math.cos((angle.Value() / 180) * Math.PI) * force.Value();
+      if (Math.abs(R_Ax) < Math.pow(10, -7)) return 0;
+      return parseFloat(R_Ax.toFixed(fixedDecimal));
+    };
+
+    // get RA_y value
+    const getR_Ay = function() {
+      const x = Math.sin((angle.Value() / 180) * Math.PI);
+
+      const R_Ay = force.Value() * x - getRE();
+
+      if (Math.abs(R_Ay) < Math.pow(10, -7)) return 0;
+
+      return parseFloat(R_Ay.toFixed(fixedDecimal));
+    };
+
+    // get RE value
+    const getRE = function() {
+      const x = Math.sin((angle.Value() / 180) * Math.PI);
+      const lengthRE = 2;
+      const lengthF = 1;
+      const RE = (force.Value() * x * lengthF) / lengthRE;
+      if (Math.abs(RE) < Math.pow(10, -7)) return 0;
+      return parseFloat(RE.toFixed(fixedDecimal));
+    };
+
+    // create R_Ax text
     b2.create(
       "text",
       [
         -2,
         -4,
         function() {
-          const R_Ax = Math.cos((angle.Value() / 180) * Math.PI) * force.Value();
-          if (Math.abs(R_Ax) < Math.pow(10, -7)) return "Sum of the x forces:" + 0;
-          return "Sum of the x forces:" + R_Ax;
+          return "Sum of the x forces:" + getR_Ax();
         }
       ],
       { fixed: true }
     );
 
-    // get RE value
-    b2.create("text", [
-      -2,
-      -6,
-      function() {
-        const x = Math.sin((angle.Value() / 180) * Math.PI);
-        const lengthRE = 2;
-        const lengthF = 1;
-        const r_e = (force.Value() * x * lengthF) / lengthRE;
-        if (Math.abs(r_e) < Math.pow(10, -7)) return "R_E: " + 0;
-        return "R_E: " + r_e;
-      }
-    ]);
-
-    // get RA_y value
+    // create R_Ay text
     b2.create("text", [
       -2,
       -8,
       function() {
-        const x = Math.sin((angle.Value() / 180) * Math.PI);
+        return "R_Ay: " + getR_Ay();
+      }
+    ]);
 
-        const lengthRE = 2;
-        const lengthF = 1;
-        const r_e = (force.Value() * x * lengthF) / lengthRE;
-
-        const r_ay = force.Value() * x - r_e;
-
-        if (Math.abs(r_ay) < Math.pow(10, -7)) return "R_Ay: " + 0;
-
-        return "R_Ay: " + r_ay;
+    // create RE text
+    b2.create("text", [
+      -2,
+      -6,
+      function() {
+        return "R_E: " + getRE();
       }
     ]);
 
@@ -69,9 +86,9 @@ export default {
     const c = b2.create("point", [10, 0], { name: "c", fixed: true });
     const d = b2.create("point", [5, 5 * Math.sqrt(3)], { name: "d", fixed: true });
     const e = b2.create("point", [-5, 5 * Math.sqrt(3)], { name: "e", fixed: true });
-    
-    
-    const F_C = b2.create(
+
+    // create FC point
+    const pointFC = b2.create(
       "point",
       [
         function() {
@@ -88,6 +105,61 @@ export default {
       { fixed: true, face: "", name: `F_C = ${force.Value()} N` }
     );
 
+    // create R_Ax point
+    const pointR_Ax = b2.create(
+      "point",
+      [
+        function() {
+          return -3 * Math.abs(getR_Ax()) - 10;
+        },
+        0
+      ],
+      {
+        fixed: true,
+        face: "",
+        name: function() {
+          return "R_{A,x} =" + getR_Ax() + "N";
+        },
+        offsetX: 10
+      }
+    );
+
+    // create R_Ay point
+    const pointR_Ay = b2.create(
+      "point",
+      [
+        -10,
+        function() {
+          return -3 * Math.abs(getR_Ay());
+        }
+      ],
+      {
+        fixed: true,
+        face: "",
+        name: function() {
+          return "R_{A,y} =" + getR_Ay() + "N";
+        }
+      }
+    );
+
+    // create RE point
+    const pointRE = b2.create(
+      "point",
+      [
+        10,
+        function() {
+          return -3 * Math.abs(getRE());
+        }
+      ],
+      {
+        fixed: true,
+        face: "",
+        name: function() {
+          return "R_{E} =" + getRE() + "N";
+        }
+      }
+    );
+
     // connect points
     b2.create("line", [a, e], { name: "lala", straightFirst: false, straightLast: false });
     b2.create("line", [a, d], { straightFirst: false, straightLast: false });
@@ -98,10 +170,22 @@ export default {
     b2.create("line", [c, d], { straightFirst: false, straightLast: false });
 
     // add force vectors
-    b2.create("line", [a, F_C], { straightFirst: false, straightLast: false, touchFirstPoint: true, lastArrow: true });
-    b2.create("line", [b, [-10, -3]], { straightFirst: false, straightLast: false, firstArrow: true, touchFirstPoint: true });
-    b2.create("line", [b, [-13, 0]], { straightFirst: false, straightLast: false, firstArrow: true, touchFirstPoint: true });
-    b2.create("line", [c, [10, -3]], { straightFirst: false, straightLast: false, touchFirstPoint: true, firstArrow: true });
+    b2.create("line", [a, pointFC], { straightFirst: false, straightLast: false, touchFirstPoint: true, lastArrow: true });
+    b2.create("line", [b, pointR_Ay], { straightFirst: false, straightLast: false, firstArrow: true, touchFirstPoint: true });
+    b2.create("line", [b, pointR_Ax], {
+      straightFirst: false,
+      straightLast: false,
+      firstArrow: function() {
+        return getR_Ax() > 0;
+      },
+      lastArrow: function() {
+        return getR_Ax() < 0;
+      },
+      touchFirstPoint: true
+    });
+    b2.create("line", [c, pointRE], { straightFirst: false, straightLast: false, touchFirstPoint: true, firstArrow: true });
+
+    // need joint analysis
   }
 };
 export const meta = {

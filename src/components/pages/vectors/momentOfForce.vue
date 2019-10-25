a23d16ea28d394873559c9829f7dfa612e57ee5c
kaiyingshan <shan_kaiying@163.com>
2019-10-25 15:29:35 -0400

    :

---
+++ src/components/pages/vectors/momentOfForce.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+<script>
+export default {
+    mounted(){
+        var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-5,10,5,-2], keepaspectratio: true, axis:true});
+
+    }
+}
+export const meta = {
+  title: "nmsl",
+  description: "nmsl"
+};
+</script>

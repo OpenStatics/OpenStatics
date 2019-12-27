5cfe7810f295cf03a531f778e68ce080d281c73a
Zichao <zichao2018@gmail.com>
2019-12-27 10:20:00 -0500

    FBD_Beam module

---
+++ src/components/pages/particles/FBD_Beam.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <cantForce :globalData="storeData" @fromChild="updateGlobal" v-if="storeData.cantMode && storeData.forceMode"></cantForce>
+    <cantMoment :globalData="storeData" @fromChild="updateGlobal" v-if="storeData.cantMode && !storeData.forceMode"></cantMoment>
+    <ssForce :globalData="storeData" @fromChild="updateGlobal" v-if="!storeData.cantMode && storeData.forceMode"></ssForce>
+    <ssMoment :globalData="storeData" @fromChild="updateGlobal" v-if="!storeData.cantMode && !storeData.forceMode"></ssMoment>
+  </div>
+</template>
+
+<script>
+import cantForce from "../../utils/FBD_Beam/cant_force";
+import cantMoment from "../../utils/FBD_Beam/cant_moment";
+import ssForce from "../../utils/FBD_Beam/ss_foce";
+import ssMoment from "../../utils/FBD_Beam/ss_moment";
+
+export default {
+  components: { cantForce, ssForce, cantMoment, ssMoment },
+  data() {
+    return {
+      Cantilever: true,
+      storeData: {
+        posVal: 0.5,
+        magVal: 1,
+        dirVal: 90,
+        cantMode: true,
+        forceMode: true
+      }
+    };
+  },
+  methods: {
+    updateGlobal(payload) {
+      this.storeData = payload;
+    }
+  }
+};
+
+export const meta = {
+  title: "Free Body Diagram of Beam",
+  description: "FBD"
+};
+</script>
---
+++ src/components/utils/FBD_Beam/cant_force.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="row">
+    force cant
+    <div id="cant_force" class="jsx-graph"></div>
+    <div>
+      <div class="border">
+        <button class="btn btn-warning mx-3">Cantilever</button>
+        <button class="btn btn-primary mx-3" @click="clickOnSS">Simple Supported</button>
+      </div>
+      <div class="border">
+        <button class="btn btn-warning mx-3">Force</button>
+        <button class="btn btn-primary mx-3" @click="clickOnMoment">Moment</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "cant_force",
+  data() {
+    return {
+      position: undefined,
+      magnitude: undefined,
+      direction: undefined
+    };
+  },
+  props: {
+    globalData: {
+      type: Object
+    }
+  },
+  mounted() {
+    const fixedDecimal = 4;
+
+    const { posVal, magVal, dirVal } = this.globalData;
+    const board = JXG.JSXGraph.initBoard("cant_force", { boundingbox: [-15, 15, 15, -15], axis: true, keepAspectRatio: true, showCopyright: false });
+    const rec_a = board.create("point", [0, -1], { fixed: true });
+    const rec_b = board.create("point", [0, 1], { fixed: true });
+    const rec_c = board.create("point", [10, 1], { fixed: true });
+    const rec_d = board.create("point", [10, -1], { fixed: true });
+    const rectangle = board.create("polygon", [rec_a, rec_b, rec_c, rec_d]);
+
+    this.magnitude = board.create("slider", [[0, -6], [10, -6], [0, magVal, 2]], { name: "Magnitude of Loading[kN]" });
+    this.position = board.create("slider", [[0, -8], [10, -8], [0, posVal, 1]], { name: "Position of Loading (m)" });
+    this.direction = board.create("slider", [[0, -10], [10, -10], [0, dirVal, 360]], { name: "Direction of Force [degree]" });
+
+    const MA_Curve = board.create(
+      "curve",
+      [
+        function(t) {
+          return -2 * Math.sin(t);
+        },
+        function(t) {
+          return -2 * Math.cos(t);
+        },
+        () => {
+          let val = this.position.Value() * this.magnitude.Value() * Math.sin((this.direction.Value() / 180) * Math.PI);
+          return ((val * 3) / 8 + 0.5) * Math.PI;
+        },
+        () => {
+          let val = -this.position.Value() * this.magnitude.Value() * Math.sin((this.direction.Value() / 180) * Math.PI);
+          return ((val * 3) / 8 + 0.5) * Math.PI;
+        }
+      ],
+      {
+        strokeWidth: 5,
+        firstArrow: true
+      }
+    );
+
+    const MA_Text = board.create("text", [-3, 0.5, "M_A"]);
+
+    const F_0 = board.create(
+      "point",
+      [
+        () => {
+          return this.position.Value() * 10;
+        },
+        0
+      ],
+      { visible: false }
+    );
+    const F_1 = board.create(
+      "point",
+      [
+        () => {
+          return this.magnitude.Value() * Math.cos((this.direction.Value() / 180) * Math.PI) * 2 + F_0.X();
+        },
+        () => {
+          return this.magnitude.Value() * Math.sin((this.direction.Value() / 180) * Math.PI) * 2 + F_0.Y();
+        }
+      ],
+      {
+        visible: false
+      }
+    );
+    const forceLine = board.create("line", [F_0, F_1], { straightFirst: false, straightLast: false, firstArrow: true });
+    const forceLineLabel = board.create("text", [0.5, 0, "F"], { anchor: forceLine });
+
+    const MA = board.create("text", [
+      -10,
+      -2,
+      () => {
+        let val = -this.position.Value() * this.magnitude.Value() * Math.sin((this.direction.Value() / 180) * Math.PI);
+        if (Math.abs(val) < Math.pow(10, -7)) val = 0;
+        return "M_A = " + parseFloat(val.toFixed(fixedDecimal)) + "kNm";
+      }
+    ]);
+    const Ax = board.create("text", [
+      -10,
+      -4,
+      () => {
+        let val = this.magnitude.Value() * Math.cos((this.direction.Value() / 180) * Math.PI);
+        if (Math.abs(val) < Math.pow(10, -7)) val = 0;
+        return "A_x = " + parseFloat(val.toFixed(fixedDecimal)) + "kN";
+      }
+    ]);
+    const Ay = board.create("text", [
+      -10,
+      -6,
+      () => {
+        let val = this.magnitude.Value() * Math.sin((this.direction.Value() / 180) * Math.PI);
+        if (Math.abs(val) < Math.pow(10, -7)) val = 0;
+        return "A_y = " + parseFloat(val.toFixed(fixedDecimal)) + "kN";
+      }
+    ]);
+  },
+  methods: {
+    clickOnSS() {
+      const posVal = this.position.Value();
+      const magVal = this.magnitude.Value();
+      const dirVal = this.direction.Value();
+      const cantMode = false;
+      const forceMode = true;
+      this.$emit("fromChild", { posVal, magVal, dirVal, cantMode, forceMode });
+    },
+    clickOnMoment() {
+      const posVal = this.position.Value();
+      const magVal = this.magnitude.Value();
+      const dirVal = this.direction.Value();
+      const cantMode = true;
+      const forceMode = false;
+      this.$emit("fromChild", { posVal, magVal, dirVal, cantMode, forceMode });
+    }
+  }
+};
+</script>

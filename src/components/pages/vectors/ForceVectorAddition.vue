37f2354b813d546c2208fec468d1b6bd8969b662
Zichao <zichao2018@gmail.com>
2020-01-12 11:09:04 -0500

    finish force vector addition

--- src/components/pages/vectors/ForceVectorAddition.vue
+++ src/components/pages/vectors/ForceVectorAddition.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <div id="box1" class="jsx-graph2 my-2"></div>
-    <div id="box2" class="jsx-graph my-2 text-center"></div>
+    <h1 class="text-danger text-center my-4">Vector Addition of Forces</h1>
+    <div class="row">
+      <div id="vecaddition" class="jsx-graph col-xl mx-2"></div>
+      <div class="col-xl mx-2">
+        <VecAddText></VecAddText>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import VecAddText from "../../utils/ForceVectorAddition/VecAddText";
 export default {
+  components: { VecAddText },
   data() {
     return {
       board: null,
@@ -14,70 +21,107 @@ export default {
     };
   },
   mounted() {
-    // create three boards: b1 is the control and b2, b3 are the display
-    const b1 = JXG.JSXGraph.initBoard("box1", { boundingbox: [-10, 10, 10, -10] });
-    const b2 = JXG.JSXGraph.initBoard("box2", { boundingbox: [-15, 15, 15, -15], axis: true, keepAspectRatio: true });
-    // const b3 = JXG.JSXGraph.initBoard("box3", { boundingbox: [-5, 5, 5, -5] });
-    b1.addChild(b2);
+    const multiplier = 2.5;
+
+    const board = JXG.JSXGraph.initBoard("vecaddition", { boundingbox: [-15, 15, 15, -15], axis: true, keepAspectRatio: true, showCopyright: false });
 
     // set slider for force and angle
-    const force_a = b1.create("slider", [[1, -2], [6, -2], [0, 1, 5]], { name: "Force A" });
-    const angle_a = b1.create("slider", [[1, -4], [6, -4], [0, 0, 360]], { name: "Angle A" });
-    const force_b = b1.create("slider", [[1, -6], [6, -6], [0, 1, 5]], { name: "Force B" });
-    const angle_b = b1.create("slider", [[1, -8], [6, -8], [0, 0, 360]], { name: "Angle B" });
+    const force_a = board.create("slider", [[-12, -6], [-6, -6], [0, 1, 5]], { name: "Force 1" });
+    const inputA = board.create("input", [-12, -4, "", "F1(N): "], { cssStyle: "width: 100px" });
+    const buttonA = board.create("button", [
+      -6,
+      -4,
+      "Update",
+      () => {
+        if (Number(inputA.Value())) force_a.setValue(Number(inputA.Value()));
+      }
+    ]);
+
+    const angle_a = board.create("slider", [[-12, -10], [-6, -10], [0, 60, 360]], { name: "Angle 1" });
+    const inputAA = board.create("input", [-12, -8, "", "&Theta;1 (degree): "], { cssStyle: "width: 100px" });
+    const buttonAA = board.create("button", [
+      -6,
+      -8,
+      "Update",
+      () => {
+        if (Number(inputAA.Value())) angle_a.setValue(Number(inputAA.Value()));
+      }
+    ]);
+
+    const force_b = board.create("slider", [[1, -6], [7, -6], [0, 1, 5]], { name: "Force 2" });
+    const inputB = board.create("input", [1, -4, "", "F2 (N): "], { cssStyle: "width: 100px" });
+    const buttonB = board.create("button", [
+      8,
+      -4,
+      "Update",
+      () => {
+        if (Number(inputB.Value())) angle_a.setValue(Number(inputB.Value()));
+      }
+    ]);
+
+    const angle_b = board.create("slider", [[1, -10], [6, -10], [0, 0, 360]], { name: "Angle 2" });
+    const inputBB = board.create("input", [1, -8, "", "&Theta;2 (degree): "], { cssStyle: "width: 100px" });
+    const buttonBB = board.create("button", [
+      8,
+      -8,
+      "Update",
+      () => {
+        if (Number(inputBB.Value())) angle_a.setValue(Number(inputBB.Value()));
+      }
+    ]);
 
     // create two points for reference
-    const origin_point = b2.create("point", [0, 0], { fixed: true }, { name: "O" });
-    const end_point_a = b2.create(
+    const origin_point = board.create("point", [0, 0], { fixed: true, name: "O" });
+    const end_point_a = board.create(
       "point",
       [
         function() {
-          return Math.cos((angle_a.Value() / 180) * Math.PI) * force_a.Value();
+          return Math.cos((angle_a.Value() / 180) * Math.PI) * force_a.Value() * multiplier;
         },
         function() {
-          return Math.sin((angle_a.Value() / 180) * Math.PI) * force_a.Value();
+          return Math.sin((angle_a.Value() / 180) * Math.PI) * force_a.Value() * multiplier;
         }
       ],
       { name: "F1" }
     );
-    const end_point_b = b2.create(
+    const end_point_b = board.create(
       "point",
       [
         function() {
-          return Math.cos((angle_b.Value() / 180) * Math.PI) * force_b.Value();
+          return Math.cos((angle_b.Value() / 180) * Math.PI) * force_b.Value() * multiplier;
         },
         function() {
-          return Math.sin((angle_b.Value() / 180) * Math.PI) * force_b.Value();
+          return Math.sin((angle_b.Value() / 180) * Math.PI) * force_b.Value() * multiplier;
         }
       ],
       { name: "F2" }
     );
 
-    const end_point_r = b2.create(
+    const end_point_r = board.create(
       "point",
       [
         function() {
           var a_x = Math.cos((angle_a.Value() / 180) * Math.PI) * force_a.Value();
           var b_x = Math.cos((angle_b.Value() / 180) * Math.PI) * force_b.Value();
           var angle = (angle_a.Value() + angle_b.Value()) / 2;
-          return a_x + b_x;
+          return (a_x + b_x) * multiplier;
         },
         function() {
           var a_y = Math.sin((angle_a.Value() / 180) * Math.PI) * force_a.Value();
           var b_y = Math.sin((angle_b.Value() / 180) * Math.PI) * force_b.Value();
           var angle = (angle_a.Value() + angle_b.Value()) / 2;
-          return a_y + b_y;
+          return (a_y + b_y) * multiplier;
         }
       ],
       { name: "FR" }
     );
 
     // create vectors
-    const vector_a = b2.create("line", [origin_point, end_point_a], { straightFirst: false, straightLast: false, lastArrow: true });
-    const vector_b = b2.create("line", [origin_point, end_point_b], { straightFirst: false, straightLast: false, lastArrow: true });
-    const vector_r = b2.create("line", [origin_point, end_point_r], { straightFirst: false, straightLast: false, lastArrow: true });
-    var show_parallolegram = b1.create("checkbox", [8, -2, "Show Parallolegram"], {});
-    const line_a = b2.create("line", [end_point_a, end_point_r], {
+    const vector_a = board.create("line", [origin_point, end_point_a], { straightFirst: false, straightLast: false, lastArrow: true });
+    const vector_b = board.create("line", [origin_point, end_point_b], { straightFirst: false, straightLast: false, lastArrow: true });
+    const vector_r = board.create("line", [origin_point, end_point_r], { straightFirst: false, straightLast: false, lastArrow: true });
+    var show_parallolegram = board.create("checkbox", [-10, 3, "Show Parallolegram"], {});
+    const line_a = board.create("line", [end_point_a, end_point_r], {
       straightFirst: false,
       straightLast: false,
       lastArrow: true,
@@ -85,7 +129,7 @@ export default {
         return show_parallolegram.Value();
       }
     });
-    const line_b = b2.create("line", [end_point_b, end_point_r], {
+    const line_b = board.create("line", [end_point_b, end_point_r], {
       straightFirst: false,
       straightLast: false,
       lastArrow: true,
@@ -100,14 +144,3 @@ export const meta = {
   description: "from JSX graph"
 };
 </script>
-
-<style>
-.jsx-graph {
-  width: 800px !important;
-  height: 800px !important;
-}
-.jsx-graph2 {
-  width: 100% !important;
-  height: 30vh !important;
-}
-</style>
---
+++ src/components/utils/ForceVectorAddition/VecAddText.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="my-3">
+    Experimental evidence has shown that a force is a vector quantity since it has a specified magnitude, direction, and sense and it adds according
+    to the parallelogram law. Two common problems in statics involve either finding the resultant force, knowing its components, or resolving a known
+    force into two components. We will now describe how each of these problems is solved using the parallelogram law.
+    <br /><br />
+
+    Given two forces, <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>1</sub> and <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>2</sub>, we know their magnitudes <i>F</i><sub>1</sub>, <i>F</i><sub>2</sub> and directions <i>&Theta;</i><sub>1</sub>, <i>&Theta;</i
+    ><sub>2</sub>. The two forces <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>1</sub> and <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>2</sub> can be added together to form the resultant force <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>R</sub> = <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>1</sub> + <i>F</i
+    ><span
+      style="position: relative;
+        bottom: 1.3ex;
+        letter-spacing: -2ex;
+        right: 1.4ex"
+      >&rarr;</span
+    ><sub>2</sub>. The adding process is shown below.<br /><br />
+
+    From this construction, we can apply the law of cosines or the law of sines to the triangle in order to obtain the magnitude of the resultant
+    force and its direction. We know that |<i>&Theta;</i><sub>1</sub> - <i>&Theta;</i><sub>2</sub>| = &alpha; + &beta; = &pi; - &gamma;, thus we have
+    &gamma; = &pi; - |<i>&Theta;</i><sub>1</sub> - <i>&Theta;</i><sub>2</sub>|. Using law of cosines we know the magnitude of resultant force will be
+    <br /><br />
+
+    \[\vec{F}_R = \sqrt{F_1^2 +F_2^2-2F_1F_2cos\gamma}\] \[=\sqrt{F_1^2 +F_2^2-2F_1F_2cos(\pi - |\theta_1 - \theta_2|)}\]
+    \[=\sqrt{F_1^2+F_2^2+2F_1F_2cos(\theta_1-\theta_2)}\] Thus, we get the magnitude \(F_R\). While for direction, we first get the angles \(\alpha\),
+    \(\beta\) by the law of sines, <br />
+    \[\frac{F_1}{sin \alpha} = \frac{F_2}{sin \beta} = \frac{F_R}{sin \gamma}\] \[\Rightarrow \alpha = sin^{-1}(\frac{F_1}{F_R}sin\gamma), \beta =
+    sin^{-1}(\frac{F_2}{F_R}sin\gamma)\] Once we get \(\alpha\),\(\beta\), we can get the magnitude direction \(\theta\) by the geometry: \[ \theta=
+    \begin{cases} \theta_1 + \beta ,& \text{if } \theta_1 \lt \theta_2\\ \theta_2 + \alpha ,& \text{if } \theta_1 \geq \theta_2 \end{cases} \] Thus we
+    know the magnitude as well as direction of the resultant force \(\vec{F}_R\).
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Force Vector Addition"
+};
+</script>

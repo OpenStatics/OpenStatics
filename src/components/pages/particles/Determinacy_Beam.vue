cdeb1b0197760fbc4e69ddad7a296def9f6925fe
Zichao <zichao2018@gmail.com>
2020-02-26 11:11:17 -0500

    clean code

--- src/components/pages/particles/Determinacy_Beam.vue
+++ src/components/pages/particles/Determinacy_Beam.vue
@@ -1,40 +1,16 @@
 <template>
   <div>
     <h1 class="text-danger text-center my-4">Determinacy of Beams</h1>
-    <fix :globalData="storeData" @fromChild="updateGlobal" v-if="storeData.fix"></fix>
-    <pin :globalData="storeData" @fromChild="updateGlobal" v-if="storeData.pin"></pin>
-    <roller :globalData="storeData" @fromChild="updateGlobal" v-if="storeData.roller"></roller>
+    <fix></fix>
   </div>
 </template>
 
 <script>
 import fix from "../../utils/Determinacy_Beam/fix";
-import pin from "../../utils/Determinacy_Beam/pin";
-import roller from "../../utils/Determinacy_Beam/roller";
+
 export default {
   name: "Determinacy",
-  components: { fix, pin, roller },
-  data() {
-    return {
-      storeData: {
-        posVal: 0.3,
-        magVal: 1,
-        dirVal: 90,
-        posMoment: 0.6,
-        magMoment: 1,
-        dirMoment: true,
-        fix: true,
-        pin: false,
-        roller: false,
-        showReactive: false
-      }
-    };
-  },
-  methods: {
-    updateGlobal(payload) {
-      this.storeData = payload;
-    }
-  }
+  components: { fix }
 };
 export const meta = {
   title: "Determinacy of Beams",

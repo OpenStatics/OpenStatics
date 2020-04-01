a2b18af460ed521173be0b982a3f2f24f2bde74b
Zichao <zichao2018@gmail.com>
2020-03-31 23:12:23 -0400

    set up general structure of friction module

--- src/components/pages/particles/FrictionOnASlope.vue
+++ src/components/pages/particles/FrictionOnASlope.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    <div id="friction" class="jsx-graph"></div>
+  <div style="margin:20px">
+    <h1 class="text-danger text-center my-4">Friction on a slope</h1>
+    <SlopeText></SlopeText>
+    <div class="row">
+      <div class="col-xl-6 mx-2">
+        <div class="ml-5 my-4">
+          <button class="btn btn-primary">gafd</button>
+        </div>
+        <div id="control" style="height:500px;width:100%" class="mx-2"></div>
+      </div>
+      <div id="friction" class="jsx-graph col-xl mx-2"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import SlopeText from "../../utils/slope/SlopeText";
+
 export default {
   data() {
     return {
@@ -19,10 +31,92 @@ export default {
       toggle: false
     };
   },
+
+  components: { SlopeText },
+
   mounted() {
+    // initial values
+    const multiplier = 4;
+    const fixedDecimal = 2;
+    const fontSize = 20;
+    const strokeWidth = 3;
+    const dash = 3;
+    const fixed = true;
+    const withLabel = false;
+
+    // create board
     const board = JXG.JSXGraph.initBoard("friction", { boundingbox: [-15, 15, 15, -15], keepAspectRatio: true, showCopyright: false });
-    this.mu = board.create("slider", [[-5, 10], [0, 10], [0, 1, 1.5]], { name: "&mu;" });
-    this.m = board.create("slider", [[-5, 8], [0, 8], [1, 1, 10]], { name: "m" });
+    const board_control = JXG.JSXGraph.initBoard("control", {
+      boundingbox: [0, 15, 15, 0],
+      showCopyright: false
+    });
+    board_control.addChild(board);
+
+    // create sliders
+    this.mu = board_control.create(
+      "slider",
+      [
+        [2, 13],
+        [12, 13],
+        [0, 1, 1.5]
+      ],
+      { withLabel }
+    );
+    board_control.create(
+      "text",
+      [
+        3,
+        14,
+        () => {
+          const value = parseFloat(this.mu.Value().toFixed(fixedDecimal));
+          return "&mu;: " + value;
+        }
+      ],
+      { fontSize, fixed }
+    );
+    const input1 = board_control.create("input", [7, 14, "", ""], { cssStyle: "width: 50px" });
+    board_control.create("button", [
+      8,
+      14,
+      "Update",
+      () => {
+        if (Number(input1.Value())) this.mu.setValue(Number(input1.Value()));
+      }
+    ]);
+
+    this.m = board_control.create(
+      "slider",
+      [
+        [2, 11],
+        [12, 11],
+        [1, 1, 10]
+      ],
+      { withLabel }
+    );
+    board_control.create(
+      "text",
+      [
+        3,
+        12,
+        () => {
+          const value = parseFloat(this.m.Value().toFixed(fixedDecimal));
+          return "m: " + value;
+        }
+      ],
+      { fontSize, fixed }
+    );
+    const input2 = board_control.create("input", [7, 12, "", ""], { cssStyle: "width: 50px" });
+    board_control.create("button", [
+      8,
+      12,
+      "Update",
+      () => {
+        if (Number(input2.Value())) this.m.setValue(Number(input2.Value()));
+      }
+    ]);
+
+    // actual content
+
     this.dummy = board.create("point", [5, 5], { visible: false });
 
     const x_axis_point = board.create("point", [8, 0], { visible: false });
---
+++ src/components/utils/slope/SlopeText.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-3">
+    The module below shows how the friction changes on a slope. 
+    <br />
+    When a block is statistically placed on a slope with inclined angle <vue-mathjax formula="(\theta)" /> and
+    coefficient of friction <vue-mathjax formula="(\mu)" />, the friction provided by the slope is a static frictional force; 
+    as <vue-mathjax formula="(\theta)" /> increases gradually and exceeds the critical value, <vue-mathjax formula="(\theta_0 = \arctan(\mu))" />
+    , the statistic balance is broken and the block begins to move, with the static frictional force changed to kinetic frictional force. 
+    While in the original balance state, changing the mass of the block, m, cannot break the balance.
+    <br />
+    Module below shows these ideas where <vue-mathjax formula="(\mu)" /> and m are variable and <vue-mathjax formula="(\theta)" /> can be 
+    increased automatically to <vue-mathjax formula="(\theta_0)" />. Play withthese parameters to see how the FBD and balance change.
+  </div>
+</template>
+<script>
+export default {
+  name: "Slope_Text"
+};
+</script>

c55fa20655e64c9f06e5be5688bdc8cf0d060d28
Jason Watkins <jmw4dx@virginia.edu>
2020-04-14 22:30:44 -0400

    Spacing and colors

--- src/components/utils/particle_equilibrium/particle_equilibrium.vue
+++ src/components/utils/particle_equilibrium/particle_equilibrium.vue
@@ -58,8 +58,10 @@ export default {
       showZoom: false
     });
 
-    const incr = 5;
-    const start = 5;
+    const incrV = 6;
+    const incrV_Box = 5;
+    const incrH = 5;
+    const start = 13;
     const left = -14;
     const right = -5;
     const label_size = 16;
@@ -67,8 +69,8 @@ export default {
     this.sliders.time = this.box_sliders.create(
       "slider",
       [
-        [left, start + incr * 0.5],
-        [right, start + incr * 0.5],
+        [left, start + incrV * 0],
+        [right, start + incrV * 0],
         [0, 0, 15]
       ],
       { name: "Time (s)", withTicks: false, label: { fontSize: label_size } }
@@ -76,66 +78,76 @@ export default {
     this.sliders.f1_force = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 2],
-        [right, start - incr * 2],
+        [left, start - incrV * 2],
+        [right, start - incrV * 2],
         [0, 0, 10]
       ],
       {
         name: "F_1 (N)",
         withTicks: false,
-        color: "red",
+        strokeColor: "black",
+        fillColor: "red",
         label: { color: "red", fontSize: label_size },
+        highline: { color: "red" },
         baseline: { color: "red" }
       }
     );
     this.sliders.f1_angle = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 3],
-        [right, start - incr * 3],
+        [left, start - incrV * 3],
+        [right, start - incrV * 3],
         [0, 0, 360]
       ],
       {
         name: "\u03B8_1 (\u00B0)",
         withTicks: false,
-        color: "red",
-        label: { color: "red", fontSize: label_size }
+        strokeColor: "black",
+        fillColor: "red",
+        label: { color: "red", fontSize: label_size },
+        highline: { color: "red" },
+        baseline: { color: "red" }
       }
     );
     this.sliders.f2_force = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 4],
-        [right, start - incr * 4],
+        [left, start - incrV * 4],
+        [right, start - incrV * 4],
         [0, 0, 10]
       ],
       {
         name: "F_2 (N)",
         withTicks: false,
-        strokeColor: "blue",
+        strokeColor: "black",
+        fillColor: "blue",
         label: { color: "blue", fontSize: label_size },
-        highline: { color: "blue" }
+        highline: { color: "blue" },
+        baseline: { color: "blue" }
       }
     );
     this.sliders.f2_angle = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 5],
-        [right, start - incr * 5],
+        [left, start - incrV * 5],
+        [right, start - incrV * 5],
         [0, 0, 360]
       ],
       {
         name: "\u03B8_2 (\u00B0)",
         withTicks: false,
-        strokeColor: "blue",
-        label: { color: "blue", fontSize: label_size }
+        strokeColor: "black",
+        fillColor: "blue",
+        label: { color: "blue", fontSize: label_size },
+        highline: { color: "blue" },
+        baseline: { color: "blue" }
       }
     );
     this.sliders.f3_force = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 6],
-        [right, start - incr * 6],
+        [left, start - incrV * 6],
+        [right, start - incrV * 6],
         [0, 0, 10]
       ],
       {
@@ -151,8 +163,8 @@ export default {
     this.sliders.f3_angle = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 7],
-        [right, start - incr * 7],
+        [left, start - incrV * 7],
+        [right, start - incrV * 7],
         [0, 0, 360]
       ],
       {
@@ -160,14 +172,16 @@ export default {
         withTicks: false,
         strokeColor: "black",
         fillColor: "green",
-        label: { color: "green", fontSize: label_size }
+        label: { color: "green", fontSize: label_size },
+        highline: { color: "green" },
+        baseline: { color: "green" }
       }
     );
     this.sliders.mass = this.box_sliders.create(
       "slider",
       [
-        [left, start - incr * 8],
-        [right, start - incr * 8],
+        [left, start - incrV * 8],
+        [right, start - incrV * 8],
         [1, 1, 3]
       ],
       { name: "Mass (kg)", withTicks: false, label: { fontSize: label_size } }
@@ -185,7 +199,7 @@ export default {
 
     this.button_play = this.box_sliders.create("button", [
       left,
-      start + incr * -0.5,
+      start + incrV * 0 + incrV_Box * -0.75,
       "Play/Pause",
       () => {
         this.animation_state.play = !this.animation_state.play;
@@ -198,8 +212,8 @@ export default {
     ]);
 
     this.box_sliders.create("button", [
-      left + incr * 1.65,
-      start + incr * -0.5,
+      left + incrH * 1.65,
+      start + incrV * 0 + incrV_Box * -0.75,
       "Reverse",
       () => {
         this.animation_state.direction *= -1;
@@ -207,8 +221,8 @@ export default {
     ]);
 
     this.box_sliders.create("button", [
-      left + incr * 0,
-      start + incr * -1,
+      left + incrH * 0,
+      start + incrV * 0 + incrV_Box * -1.25,
       "x0.25",
       () => {
         this.animation_state.speed = 0.25;
@@ -217,8 +231,8 @@ export default {
     ]);
 
     this.box_sliders.create("button", [
-      left + incr * 1,
-      start + incr * -1,
+      left + incrH * 1,
+      start + incrV * 0 + incrV_Box * -1.25,
       "x0.50",
       () => {
         this.animation_state.speed = 0.5;
@@ -227,8 +241,8 @@ export default {
     ]);
 
     this.box_sliders.create("button", [
-      left + incr * 2,
-      start + incr * -1,
+      left + incrH * 2,
+      start + incrV * 0 + incrV_Box * -1.25,
       "x1.00",
       () => {
         this.animation_state.speed = 1;
@@ -237,8 +251,8 @@ export default {
     ]);
 
     this.box_sliders.create("button", [
-      left + incr * 3,
-      start + incr * -1,
+      left + incrH * 3,
+      start + incrV * 0 + incrV_Box * -1.25,
       "x1.50",
       () => {
         this.animation_state.speed = 1.5;
@@ -246,8 +260,8 @@ export default {
       }
     ]);
     this.box_sliders.create("button", [
-      left + incr * 4,
-      start + incr * -1,
+      left + incrH * 4,
+      start + incrV * 0 + incrV_Box * -1.25,
       "x2.00",
       () => {
         this.animation_state.speed = 1.5;
@@ -255,45 +269,25 @@ export default {
       }
     ]);
 
-    this.sliders.f1_force.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.f1_angle.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.f2_force.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.f2_angle.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.f3_force.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.f3_angle.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
-    this.sliders.mass.on("drag", () => {
-      this.animation_state.play = false;
-      clearInterval(this.interval);
-      this.sliders.time.setValue(0);
-    });
+    let y = () => {
+      return () => {
+        this.animation_state.play = false;
+        clearInterval(this.interval);
+        this.sliders.time.setValue(0);
+      };
+    };
+
+    this.sliders.f1_force.on("drag", y());
+    this.sliders.f1_angle.on("drag", y());
+    this.sliders.f2_force.on("drag", y());
+    this.sliders.f2_angle.on("drag", y());
+    this.sliders.f3_force.on("drag", y());
+    this.sliders.f3_angle.on("drag", y());
+    this.sliders.mass.on("drag", y());
 
     let buttonClick = (textbox, slider) => {
       return () => {
-        if (Number(textbox.Value())) {
+        if (!isNaN(textbox.Value())) {
           let val = Number(textbox.Value());
           val = Math.min(slider._smax, val);
           val = Math.max(slider._smin, val);
@@ -306,59 +300,59 @@ export default {
       };
     };
 
-    this.input_time = this.box_sliders.create("input", [right + incr * 2.6, start + incr * 0.5, "", ""], { cssStyle: "width: 58px" });
+    this.input_time = this.box_sliders.create("input", [right + incrH * 2.6, start + incrV * 0, "", ""], { cssStyle: "width: 58px" });
     this.button_time = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 0, "Update", buttonClick(this.input_time, this.sliders.time)],
+      [right + incrH * 2.6, start + incrV * 0 - incrV_Box * 0.5, "Update", buttonClick(this.input_time, this.sliders.time)],
       {}
     );
 
-    this.input_f1_force = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 2, "", ""], { cssStyle: "width: 58px" });
+    this.input_f1_force = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 2, "", ""], { cssStyle: "width: 58px" });
     this.button_f1_force = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 2.5, "Update", buttonClick(this.input_f1_force, this.sliders.f1_force)],
+      [right + incrH * 2.6, start - incrV * 2 - incrV_Box * 0.5, "Update", buttonClick(this.input_f1_force, this.sliders.f1_force)],
       {}
     );
 
-    this.input_f1_angle = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 3, "", ""], { cssStyle: "width: 58px" });
+    this.input_f1_angle = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 3, "", ""], { cssStyle: "width: 58px" });
     this.button_f1_angle = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 3.5, "Update", buttonClick(this.input_f1_angle, this.sliders.f1_angle)],
+      [right + incrH * 2.6, start - incrV * 3 - incrV_Box * 0.5, "Update", buttonClick(this.input_f1_angle, this.sliders.f1_angle)],
       {}
     );
 
-    this.input_f2_force = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 4, "", ""], { cssStyle: "width: 58px" });
+    this.input_f2_force = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 4, "", ""], { cssStyle: "width: 58px" });
     this.button_f2_force = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 4.5, "Update", buttonClick(this.input_f2_force, this.sliders.f2_force)],
+      [right + incrH * 2.6, start - incrV * 4 - incrV_Box * 0.5, "Update", buttonClick(this.input_f2_force, this.sliders.f2_force)],
       {}
     );
 
-    this.input_f2_angle = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 5, "", ""], { cssStyle: "width: 58px" });
+    this.input_f2_angle = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 5, "", ""], { cssStyle: "width: 58px" });
     this.button_f2_angle = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 5.5, "Update", buttonClick(this.input_f2_angle, this.sliders.f2_angle)],
+      [right + incrH * 2.6, start - incrV * 5 - incrV_Box * 0.5, "Update", buttonClick(this.input_f2_angle, this.sliders.f2_angle)],
       {}
     );
 
-    this.input_f3_force = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 6, "", ""], { cssStyle: "width: 58px" });
+    this.input_f3_force = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 6, "", ""], { cssStyle: "width: 58px" });
     this.button_f3_force = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 6.5, "Update", buttonClick(this.input_f3_force, this.sliders.f3_force)],
+      [right + incrH * 2.6, start - incrV * 6 - incrV_Box * 0.5, "Update", buttonClick(this.input_f3_force, this.sliders.f3_force)],
       {}
     );
 
-    this.input_f3_angle = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 7, "", ""], { cssStyle: "width: 58px" });
+    this.input_f3_angle = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 7, "", ""], { cssStyle: "width: 58px" });
     this.button_f3_angle = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 7.5, "Update", buttonClick(this.input_f3_angle, this.sliders.f3_angle)],
+      [right + incrH * 2.6, start - incrV * 7 - incrV_Box * 0.5, "Update", buttonClick(this.input_f3_angle, this.sliders.f3_angle)],
       {}
     );
 
-    this.input_mass = this.box_sliders.create("input", [right + incr * 2.6, start - incr * 8, "", ""], { cssStyle: "width: 58px" });
+    this.input_mass = this.box_sliders.create("input", [right + incrH * 2.6, start - incrV * 8, "", ""], { cssStyle: "width: 58px" });
     this.button_mass = this.box_sliders.create(
       "button",
-      [right + incr * 2.6, start - incr * 8.5, "Update", buttonClick(this.input_mass, this.sliders.mass)],
+      [right + incrH * 2.6, start - incrV * 8 - incrV_Box * 0.5, "Update", buttonClick(this.input_mass, this.sliders.mass)],
       {}
     );
 

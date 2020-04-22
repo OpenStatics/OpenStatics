d25e3a9e769333a86c8bee35e29c639bbd711569
Jason Watkins <jmw4dx@virginia.edu>
2020-04-22 15:07:07 -0400

    Labels and updating text

--- src/components/pages/methods_of_joint/methods_of_joints.vue
+++ src/components/pages/methods_of_joint/methods_of_joints.vue
@@ -1,22 +1,66 @@
 <template>
-  <div>
-    <loadedTruss />
+  <div class="container-fluid">
+    <h1 class="text-danger text-center my-4">Method of Joints</h1>
+    <div class="row">
+      <div class="col">
+        <jointText />
+      </div>
+      <div class="col">
+        <div class="row align-items-center">
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 0 }" @click="() => changeSelection(0)">
+              Truss
+            </button>
+          </div>
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 1 }" @click="() => changeSelection(1)">
+              FBD
+            </button>
+          </div>
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 2 }" @click="() => changeSelection(2)">
+              &Sigma;M<sub>A</sub>=0
+            </button>
+          </div>
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 3 }" @click="() => changeSelection(3)">
+              &Sigma;F<sub>x</sub>=0
+            </button>
+          </div>
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 4 }" @click="() => changeSelection(4)">
+              &Sigma;F<sub>y</sub>=0
+            </button>
+          </div>
+          <div class="col">
+            <button class="btn btn-primary mx-3" :class="{ 'btn-warning': currentSelection === 5 }" @click="() => changeSelection(5)">
+              Solved
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <loadedTruss ref="foo" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import loadedTrussVue from "../../utils/methods_of_joint/loadedTruss";
-
+import joint_text from "../../utils/methods_of_joint/joint_text";
 export default {
   data() {
     return { currentSelection: 0 };
   },
   components: {
-    loadedTruss: loadedTrussVue
+    loadedTruss: loadedTrussVue,
+    jointText: joint_text
   },
   methods: {
     changeSelection(nextState) {
       this.currentSelection = nextState;
+      this.$refs.foo.changeState(nextState);
       console.log(this.currentSelection);
     }
   },
--- src/components/utils/methods_of_joint/loadedTruss.vue
+++ src/components/utils/methods_of_joint/loadedTruss.vue
@@ -9,13 +9,32 @@ export default {
   components: {},
   data: () => {
     return {
-      state: 0
+      state: 0,
+      info: {
+        top: undefined,
+        middle: undefined,
+        bottom: undefined
+      },
+      infoText: {
+        top: undefined,
+        middle: undefined,
+        bottom: undefined
+      },
+      b2: undefined
     };
   },
   mounted() {
-    const scale = 3;
-    const b2 = JXG.JSXGraph.initBoard("box1", { boundingbox: [-15, 15, 15, -15], keepAspectRatio: true, showCopyright: false });
-
+    const scale = 4;
+    const b2 = JXG.JSXGraph.initBoard("box1", {
+      boundingbox: [-15, 10, 15, -15],
+      keepAspectRatio: true,
+      showCopyright: false,
+      pan: { enabled: false },
+      zoom: { wheel: false },
+      showNavigation: false,
+      showZoom: false
+    });
+    b2.canvasHeight = 600;
     const force = b2.create(
       "slider",
       [
@@ -23,7 +42,7 @@ export default {
         [0, -9],
         [1, 1, 2]
       ],
-      { name: "Load F" }
+      { name: "Load F (N)" }
     );
     const angle = b2.create(
       "slider",
@@ -32,7 +51,7 @@ export default {
         [0, -10],
         [0, 90, 180]
       ],
-      { name: "Angle(\u03b1)" }
+      { name: "Angle" }
     );
 
     const input_force = b2.create("input", [5, -9 + 0.05, "", ""], { cssStyle: "width: 50px" });
@@ -79,6 +98,12 @@ export default {
       {}
     );
 
+    const forStates = options => {
+      return () => {
+        options.includes(this.state);
+      };
+    };
+
     const offsetFactor = 11;
 
     // create points
@@ -111,7 +136,55 @@ export default {
       label: { ...labelProperties, offset: [(offsetFactor * 1) / 2, -offsetFactor] }
     });
 
-    const lineProperties = { straightFirst: false, straightLast: false, strokeWidth: 10 };
+    const triangleScale = scale / 4;
+    const triangleVert = triangleScale / 4;
+    const point_T1 = b2.create("point", [-2 * scale + triangleScale, -1 * Math.sqrt(3) * triangleScale + triangleVert], {
+      fixed: true,
+      visible: false
+    });
+    const point_T2 = b2.create("point", [-2 * scale - triangleScale, -1 * Math.sqrt(3) * triangleScale + triangleVert], {
+      fixed: true,
+      visible: false
+    });
+    const point_T3 = b2.create("point", [-2 * scale, triangleVert], { fixed: true, visible: false });
+
+    b2.create("polygon", [point_T3, point_T1, point_T2], {
+      visible: () => {
+        return this.state == 0;
+      },
+      fillColor: "green",
+      borders: {
+        strokeColor: "green",
+        strokeWidth: 2
+      },
+      fixed: true
+    });
+
+    b2.create(
+      "circle",
+      [
+        b2.create("point", [2 * scale, -0.5 * Math.sqrt(3) * triangleScale], { fixed: true, visible: false }),
+        0.5 * Math.sqrt(3) * triangleScale - triangleVert * 0.5
+      ],
+      {
+        fillColor: "green",
+        strokeColor: "grey",
+        visible: () => {
+          return this.state == 0;
+        },
+        fixed: true
+      }
+    );
+
+    const lineProperties = {
+      straightFirst: false,
+      straightLast: false,
+      strokeWidth: () => {
+        if ([1, 2, 3, 4, 5].includes(this.state)) return 4;
+        else return 10;
+      },
+      strokeColor: "green"
+    };
     for (const points of [
       [point_a, point_b],
       [point_a, point_c],
@@ -124,26 +197,229 @@ export default {
       b2.create("line", points, { ...lineProperties, visible: true });
     }
 
-    const label_top = b2.create("text", [-6, -6, ""], {
-      fontSize: 14,
+    const vectorScale = scale * 0.4;
+    const point_F = b2.create(
+      "point",
+      [
+        () => {
+          return -vectorScale * force.Value() * Math.cos((angle.Value() / 180) * Math.PI);
+        },
+        () => {
+          return -vectorScale * force.Value() * Math.sin((angle.Value() / 180) * Math.PI);
+        }
+      ],
+      { visible: false, fixed: true }
+    );
+
+    const point_Rax = b2.create(
+      "point",
+      [
+        () => {
+          if ([5].includes(this.state)) return -1 * Math.abs(vectorScale * force.Value() * Math.cos((angle.Value() / 180) * Math.PI)) - scale * 2;
+          else return -(vectorScale + scale * 2);
+        },
+        0
+      ],
+      { visible: false, fixed: true }
+    );
+
+    const point_Ray = b2.create(
+      "point",
+      [
+        -scale * 2,
+        () => {
+          if ([5].includes(this.state)) return -vectorScale * 0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI);
+          else return -vectorScale;
+        }
+      ],
+      { visible: false, fixed: true }
+    );
+
+    const point_Re = b2.create(
+      "point",
+      [
+        scale * 2,
+        () => {
+          if ([5].includes(this.state)) return -vectorScale * 0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI);
+          else return -vectorScale;
+        }
+      ],
+      { visible: false, fixed: true }
+    );
+
+    const forceVectorProperties = { straightFirst: false, straightLast: false, touchFirstPoint: true, lastArrow: true, strokeWidth: 4 };
+
+    b2.create("line", [point_c, point_F], {
+      ...forceVectorProperties,
+      strokeColor: "blue",
+      visible: () => {
+        return [0, 1, 2, 3, 4, 5].includes(this.state);
+      }
+    });
+    b2.create("line", [point_Rax, point_a], {
+      ...forceVectorProperties,
+      strokeColor: "red",
+      visible: () => {
+        return [1, 3, 4, 5].includes(this.state);
+      }
+    });
+    b2.create("line", [point_Ray, point_a], {
+      ...forceVectorProperties,
+      strokeColor: "red",
+      visible: () => {
+        return [1, 3, 4, 5].includes(this.state);
+      }
+    });
+    b2.create("line", [point_Re, point_e], {
+      ...forceVectorProperties,
+      strokeColor: "red",
+      visible: () => {
+        return [1, 2, 3, 4, 5].includes(this.state);
+      }
+    });
+
+    b2.create(
+      "text",
+      [
+        0,
+        0,
+        () => {
+          if ([1, 3, 4].includes(this.state)) return "R_{A,x}";
+          else if ([5].includes(this.state))
+            return "R_{A,x} = " + String(Math.round(force.Value() * Math.cos((angle.Value() / 180) * Math.PI) * 100) / 100) + " kN";
+          else return "";
+        }
+      ],
+      {
+        anchor: point_Rax,
+        anchorX: "right",
+        strokeColor: "red",
+        offset: [-offsetFactor * 1, offsetFactor * 0],
+        isLabel: true,
+        fontSize: 14,
+        fixed: true
+      }
+    );
+
+    b2.create(
+      "text",
+      [
+        0,
+        0,
+        () => {
+          if ([1, 3, 4].includes(this.state)) return "R_{A,y}";
+          else if ([5].includes(this.state))
+            return "R_{A,y} = " + String(Math.round(0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI) * 100) / 100) + " kN";
+          else return "";
+        }
+      ],
+      {
+        anchor: point_Ray,
+        anchorX: "middle",
+        strokeColor: "red",
+        offset: [-offsetFactor * 0, -offsetFactor * 0.75],
+        isLabel: true,
+        fontSize: 14,
+        fixed: true
+      }
+    );
+
+    b2.create(
+      "text",
+      [
+        0,
+        0,
+        () => {
+          if ([1, 2].includes(this.state)) return "R_E";
+          else if ([3, 4, 5].includes(this.state))
+            return "R_E = " + String(Math.round(0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI) * 100) / 100) + " kN";
+          else return "";
+        }
+      ],
+      {
+        anchor: point_Re,
+        anchorX: "middle",
+        strokeColor: "red",
+        offset: [offsetFactor * 0.5, -offsetFactor * 0.75],
+        isLabel: true,
+        fontSize: 14,
+        fixed: true
+      }
+    );
+
+    this.infoText = {
+      top: [
+        "",
+        "",
+        "sum of the moments about A: \u03a3 M_A = 0 = wF_C \\sin (\u03a6) - 2wR_E",
+        "sum of the x forces: \u03a3 F_x = F_C \\cos (\u03a6) = R_{A,x}",
+        "sum of the y forces: \u03a3 F_y = 0 = R_{A,y} - F_C \\sin (\u03a6) + R_E",
+        ""
+      ],
+      middle: [
+        "Every member has an equal length of w",
+        "",
+        "reaction force E: R_E = 0.5*F_C \\sin \u03a6",
+        () => {
+          return (
+            "x component of reaction force A: R_{A,x} = " +
+            String(Math.round(force.Value() * Math.cos((angle.Value() / 180) * Math.PI) * 100) / 100) +
+            " kN"
+          );
+        },
+        "y component of reaction force A: R_{A,y} = F_C \\sin (\u03a6) - R_E",
+        ""
+      ],
+      bottom: [
+        "",
+        "",
+        () => {
+          return "R_E = " + String(Math.round(0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI) * 100) / 100) + " kN";
+        },
+        "",
+        () => {
+          return "R_{A,y} = " + String(Math.round(0.5 * force.Value() * Math.sin((angle.Value() / 180) * Math.PI) * 100) / 100) + " kN";
+        },
+        ""
+      ]
+    };
+
+    const infoProperties = { fontSize: 14, fixed: true, useMathJax: false };
+
+    this.info.top = b2.create("text", [-6, -6, ""], {
+      ...infoProperties,
       visible: () => {
         return [2, 3, 4].includes(this.state);
       }
     });
 
-    const label_middle = b2.create("text", [-6, -7, ""], {
-      fontSize: 14,
+    this.info.middle = b2.create("text", [-6, -7, ""], {
+      ...infoProperties,
       visible: () => {
         return [0, 2, 3, 4].includes(this.state);
       }
     });
 
-    const label_bottom = b2.create("text", [-6, -8, ""], {
-      fontSize: 14,
+    this.info.bottom = b2.create("text", [-6, -8, ""], {
+      ...infoProperties,
       visible: () => {
         return [2, 4].includes(this.state);
       }
     });
+    this.b2 = b2;
+    this.changeState(0);
+  },
+  methods: {
+    changeState(newState) {
+      //console.log(newState);
+      this.info.top.setText(this.infoText.top[newState]);
+      this.info.middle.setText(this.infoText.middle[newState]);
+      this.info.bottom.setText(this.infoText.bottom[newState]);
+
+      this.state = newState;
+      this.b2.fullUpdate();
+      //console.log(this.infoText.bottom[newState]);
+    }
   }
 };
 export const meta = {

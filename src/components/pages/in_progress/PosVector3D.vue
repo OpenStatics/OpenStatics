3ca657cc3f7a15b046f450642f7157c1aa528819
Jason Watkins <jmw4dx@virginia.edu>
2020-08-07 14:36:14 -0400

    Position Vector module

---
+++ src/components/pages/in_progress/PosVector3D.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container-fluid">
+    <h1 class="text-danger text-center my-4">Position Vector 3D</h1>
+    <p>
+      Upper text
+    </p>
+    <div class="text-center">
+      <p>Centered text</p>
+    </div>
+    <pv />
+  </div>
+</template>
+<script>
+import position_vector_3D from "../../utils/PosVec3D/pos_vec_3D.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    pv: position_vector_3D
+  }
+};
+export const meta = {
+  title: "Position Vector 3D",
+  description: "",
+  number: 4
+};
+</script>
+
+<style>
+p {
+  font-size: 16;
+}
+</style>
--- src/components/utils/PosVec3D/pos_vec_3D.vue
+++ src/components/utils/PosVec3D/pos_vec_3D.vue
@@ -29,7 +29,11 @@ export default {
     return {
       state: 0,
       values: {
-        coords: "on"
+        pointA: "off",
+        pointB: "off",
+        pos_vec: "off",
+        comps: "off",
+        rotation: "off"
       },
       textToUpdate: {
         /* arbitrary name: {
@@ -72,7 +76,7 @@ export default {
     bL.resizeContainer(425, 700);
 
     const bR = JXG.JSXGraph.initBoard("boxRight", {
-      boundingbox: [-15, 15, 15, -15],
+      boundingbox: [-11, 11, 11, -11],
       keepAspectRatio: true,
       showCopyright: false,
       pan: { enabled: false },
@@ -83,6 +87,7 @@ export default {
     });
     // The size of the container (matches explicit css style in html)
     bR.resizeContainer(700, 700);
+    //bR.options.layer["text"] = 10;
 
     // All sliders are stored in these objects
     // They can be accessed with brackets sliders["force"] or dot notation sliders.force
@@ -139,22 +144,19 @@ export default {
           [allowed states],
           color ]*/
       /* For just text: [false, label name, mathJax] */
-      ["coords", "Coordinate Visibility", INTERVAL * 0, false, ["On", "on", "Off", "off"], [0]],
-      [
-        false,
-        "<b>y = f(x) = <span style='color:red'>a</span>x^2 + <span style='color:blue'>b</span>x + <span style='color:green'>c</span>:</b>",
-        INTERVAL * 1,
-        false
-      ],
-      ["a", "a", INTERVAL * 1.5, true, [-3, 0, 3], [0], "red"],
-      ["b", "b", INTERVAL * 2.5, true, [-3, 0, 3], [0], "blue"],
-      ["c", "c", INTERVAL * 3.5, true, [-3, 0, 3], [0], "green"],
-      [false, "<b>Range of element, from x_1 to x_2:</b>", INTERVAL * 4.75, false],
-      ["x1", "x_1", INTERVAL * 5.5, true, [-3, 0, 3], [0], "black"],
-      ["x2", "x_2", INTERVAL * 6.5, true, [-3, 3, 3], [0], "black"],
-      [false, "<b>Translate graph:</b>", INTERVAL * 7.75, false],
-      ["xT", "x", INTERVAL * 8.5, true, [-3, 0, 3], [0], "black"],
-      ["yT", "y", INTERVAL * 9.5, true, [-3, 0, 3], [0], "black"]
+      ["rotation", "Rotate Axes", INTERVAL * -0.5, false, ["On", "on", "Off", "off"], [0]],
+      [false, "<b>Point A Coordinates</b>:", INTERVAL * 0, false],
+      ["xA", "x_A", INTERVAL * 0.5, true, [-5, -3, 5], [0], "blue"],
+      ["yA", "y_A", INTERVAL * 1.5, true, [-5, -3, 5], [0], "blue"],
+      ["zA", "z_A", INTERVAL * 2.5, true, [-5, -3, 5], [0], "blue"],
+      ["pointA", "Show point A", INTERVAL * 3.5, false, ["On", "on", "Off", "off"], [0]],
+      [false, "<b>Point B Coordinates</b>:", INTERVAL * 4.25, false],
+      ["xB", "x_B", INTERVAL * 4.75, true, [-5, 3, 5], [0], "green"],
+      ["yB", "y_B", INTERVAL * 5.75, true, [-5, 3, 5], [0], "green"],
+      ["zB", "z_B", INTERVAL * 6.75, true, [-5, 3, 5], [0], "green"],
+      ["pointB", "Show point B", INTERVAL * 7.75, false, ["On", "on", "Off", "off"], [0]],
+      ["pos_vec", "<b>Position Vector</b>", INTERVAL * 8.5, false, ["On", "on", "Off", "off"], [0]],
+      ["comps", "Components", INTERVAL * 9, false, ["On", "on", "Off", "off"], [0]]
     ]) {
       bL.create("text", [-15, TOP_Y + data[2], data[1] + (data[0] != false ? ":" : "")], {
         fontSize: LABEL_SIZE,
@@ -271,177 +273,286 @@ export default {
       }
     }
 
-    let convX = (x, g) => {
-      let zero = g.corner.x + g.size.x * g.anchor.x;
-      return zero + (x / g.scale.x) * g.size.x;
+    let glideVal = root => {
+      // convert position on circle to angle in radians
+      let cS = circleSlides[root];
+      let angleRad = Math.acos((cS.glider.X() - cS.center.X()) / cS.circle.Radius());
+      angleRad = cS.glider.Y() - cS.center.Y() >= 0 ? angleRad : Math.PI * 2 - angleRad;
+      return (Math.round((angleRad * 180) / Math.PI) * Math.PI) / 180;
     };
 
-    let convY = (y, g) => {
-      let zero = g.corner.y + g.size.y * g.anchor.y;
-      return zero + (y / g.scale.y) * g.size.y;
-    };
+    // Completely functional, can't find a usage
+    // let setGlideVal = (root, deg) => {
+    //   let cS = circleSlides[root];
+    //   let radius = cS.circle.Radius();
+    //   cS.glider.setPositionDirectly(JXG.COORDS_BY_USER, [
+    //     radius * Math.cos((deg * Math.PI) / 180) + cS.center.X(),
+    //     radius * Math.sin((deg * Math.PI) / 180) + cS.center.Y()
+    //   ]);
+    // };
 
-    let convXY = (x, y, g) => {
-      return [convX(x, g), convY(y, g)];
-    };
+    let circleSlides = {};
+    for (let data of [
+      ["tx", 8, -8, 1, 250, "\u03b8_x"],
+      ["ty", 5, -8, 1, 180, "\u03b8_y"],
+      ["tz", 2, -8, 1, 150, "\u03b8_z"]
+    ]) {
+      circleSlides[data[0]] = {};
+      let cS = circleSlides[data[0]];
+      cS.center = bR.create("point", [data[1], data[2]], { fixed: true, visible: false, showInfobox: false });
+      cS.circle = bR.create("circle", [cS.center, data[3]], { fixed: true, visible: valCheck("rotation", "on"), strokeColor: "black" });
+      cS.glider = bR.create(
+        "glider",
+        [data[3] * Math.cos((data[4] * Math.PI) / 180) + data[1], data[3] * Math.sin((data[4] * Math.PI) / 180) + data[2], cS.circle],
+        {
+          name: data[5],
+          showInfobox: false,
+          visible: valCheck("rotation", "on"),
+          strokeColor: "purple",
+          fillColor: "purple",
+          label: { strokeColor: "purple" }
+        }
+      );
+      cS.label = bR.create("text", [data[1], data[2], ""], {
+        digits: 0,
+        fontSize: 12,
+        fixed: true,
+        anchorX: "middle",
+        anchorY: "middle",
+        visible: valCheck("rotation", "on"),
+        strokeColor: "purple"
+      });
+      this.textToUpdate[data[0] + "_label"] = {
+        object: cS.label,
+        formula: () => {
+          return Math.round((glideVal(data[0]) * 180) / Math.PI);
+        }
+      };
+    }
+
+    //setGlideVal("tz", 0);
 
     // Handles all calculations
     let comp = {};
     comp.zero = () => {
       return 0;
     };
-
-    let graphs = {
-      /*
-      arbitrary name of graph
-        corner: lower left point coordinates
-        size: length of graph on each side
-        delta: tick distancing
-        scale: length of relative space to exist on each axis
-        anchor: 0 - lowest point, 1 - highest point, 0.5 - middle, location of 0
-        axis: Axes labels
-        Title: title label
-        func: what the curve should use
-        Color: undefined for no curve, color for curve
-      */
-      //   large: {
-      //     corner: { x: -13, y: -13 },
-      //     size: { x: 26, y: 26 },
-      //     delta: { x: 1, y: 1 },
-      //     scale: { x: 3.25 * 2, y: 3.25 * 2 },
-      //     anchor: { x: 0.5, y: 0.5 },
-      //     axis: { x: "", y: "" },
-      //     title: "",
-      //     visible: true,
-      //     func: comp.fX,
-      //     color: "blue",
-      //     outlines: false
-      //   }
+    comp.radians = deg => {
+      return (deg * Math.PI) / 180;
+    };
+    comp.degrees = rad => {
+      return (rad * 180) / Math.PI;
+    };
+    // comp.THETA = { x: 250, y: 180, z: 150 };
+    // comp.ROT = {};
+    // for (let axis of Object.keys(comp.THETA)) comp.ROT[axis] = comp.radians(comp.THETA[axis]);
+    comp.x = (a, b, c) => {
+      return (a * Math.cos(glideVal("tz")) - b * Math.sin(glideVal("tz"))) * Math.cos(glideVal("ty")) - c * Math.sin(glideVal("ty"));
+    };
+    comp.y = (a, b, c) => {
+      return (b * Math.cos(glideVal("tz")) + a * Math.sin(glideVal("tz"))) * Math.cos(glideVal("tx")) - c * Math.sin(glideVal("tx"));
+    };
+    comp.xy = (a, b, c) => {
+      return [comp.x(a, b, c), comp.y(a, b, c)];
     };
 
-    // Setup the graphs
-    for (let key of Object.keys(graphs)) {
-      // The graph of focus
-      let g = graphs[key];
+    comp.xDiff = () => {
+      return sliders.xB.Value() - sliders.xA.Value();
+    };
+    comp.yDiff = () => {
+      return sliders.yB.Value() - sliders.yA.Value();
+    };
+    comp.zDiff = () => {
+      return sliders.zB.Value() - sliders.zA.Value();
+    };
 
-      // Generate the lines
-      g.lines = {};
-      for (let data of [
-        ["left", [g.corner.x, g.corner.y], [g.corner.x, g.corner.y + g.size.y]],
-        ["top", [g.corner.x, g.corner.y + g.size.y], [g.corner.x + g.size.x, g.corner.y + g.size.y]],
-        ["right", [g.corner.x + g.size.x, g.corner.y], [g.corner.x + g.size.x, g.corner.y + g.size.y]],
-        ["bottom", [g.corner.x, g.corner.y], [g.corner.x + g.size.x, g.corner.y]]
-      ]) {
-        g.lines[data[0]] = bR.create("line", [data[1], data[2]], {
-          straightFirst: false,
-          straightLast: false,
-          fixed: true,
-          strokeWidth: 3,
+    for (let data of [
+      /* [x offset from left side, y coordinate, [label, function, unit], valCheck arg 1, valCheck arg 2, color] */
+      [0, INTERVAL * 9.5, ["x_B - x_A", comp.xDiff, ""], "comps", "on", "red"],
+      [10, INTERVAL * 9.5, ["y_B - y_A", comp.yDiff, ""], "comps", "on", "red"],
+      [20, INTERVAL * 9.5, ["z_B - z_A", comp.zDiff, ""], "comps", "on", "red"]
+    ]) {
+      bL.create(
+        "text",
+        [
+          -15 + data[0],
+          TOP_Y + data[1],
+          () => {
+            let x = data[2][1]();
+            return (
+              "<span style='color:" +
+              data[5] +
+              "'><i>" +
+              data[2][0] +
+              "</i> = </span>" +
+              Math.round(x * 1000, 3) / 1000 +
+              " <span style='color:" +
+              data[5] +
+              "'><b>" +
+              data[2][2] +
+              "</b></span>"
+            );
+          }
+        ],
+        { fontSize: LABEL_SIZE, fixed: true, visible: valCheck(data[3], data[4]), anchorX: "left", anchorY: "top" }
+      );
+    }
+
+    let points = {};
+    const hiddenProps = { fixed: true, visible: false, showInfobox: false };
+    const hiddenLabelProps = { fixed: true, visible: true, withLabel: true, size: 0, showInfobox: false };
+    const lineSegProps = { straightFirst: false, straightLast: false };
+    const COPY = bR.create("transform", [0, 0], { type: "translate" });
+
+    // axes (x, y, z)
+    const axisLength = 10;
+    const axisProps = { strokeColor: "black", strokeWidth: 3, ...lineSegProps, lastArrow: true };
+    bR.create(
+      "line",
+      [
+        () => {
+          return comp.xy(-axisLength, 0, 0);
+        },
+        () => {
+          return comp.xy(axisLength, 0, 0);
+        }
+      ],
+      { ...axisProps, point2: { ...hiddenLabelProps, name: "x" } }
+    );
+    bR.create(
+      "line",
+      [
+        () => {
+          return comp.xy(0, -axisLength, 0);
+        },
+        () => {
+          return comp.xy(0, axisLength, 0);
+        }
+      ],
+      { ...axisProps, point2: { ...hiddenLabelProps, name: "y" } }
+    );
+    bR.create(
+      "line",
+      [
+        () => {
+          return comp.xy(0, 0, -axisLength);
+        },
+        () => {
+          return comp.xy(0, 0, axisLength);
+        }
+      ],
+      { ...axisProps, point2: { ...hiddenLabelProps, name: "z" } }
+    );
+
+    for (let i = 0; i < 8; i++) {
+      // Each i value converted to binary: 7 -> 111
+      // Bit 1 -> x, Bit 2 -> y, Bit 3 -> z
+      // 1 -> B, 0 -> A
+      let d1 = (i & 4) / 4;
+      let d2 = (i & 2) / 2;
+      let d3 = (i & 1) / 1;
+      let s1 = d1 === 1 ? "xB" : "xA";
+      let s2 = d2 === 1 ? "yB" : "yA";
+      let s3 = d3 === 1 ? "zB" : "zA";
+      // console.log([d1, d2, d3]);
+      points[String(i)] = bR.create(
+        "point",
+        [
+          () => {
+            return comp.x(sliders[s1].Value(), sliders[s2].Value(), sliders[s3].Value());
+          },
+          () => {
+            return comp.y(sliders[s1].Value(), sliders[s2].Value(), sliders[s3].Value());
+          }
+        ],
+        //{ visible: valCheck("comps", "on"), name: String(i) }
+        { ...hiddenProps }
+      );
+    }
+
+    // Dotted lines
+    for (let data of [
+      ["0", "1", false],
+      ["0", "2", false],
+      ["1", "3", false],
+      ["2", "3", false],
+      ["1", "5", false],
+      ["3", "7", false],
+      ["5", "7", false],
+      ["2", "6", false],
+      ["4", "5", false],
+      ["0", "4", true],
+      ["4", "6", true],
+      ["6", "7", true]
+    ]) {
+      if (data[2] === false) {
+        bR.create("line", [points[data[0]], points[data[1]]], {
+          visible: valCheck("comps", "on"),
+          ...lineSegProps,
           strokeColor: "black",
-          visible: g.visible
+          strokeWidth: 3,
+          dash: 2
         });
-      }
-
-      // Generate the ticks
-      bR.create("ticks", [g.lines.bottom, g.delta.x], {
-        anchor: g.anchor.x,
-        includeBoundaries: true,
-        drawLabels: true,
-        drawZero: true,
-        fixed: true,
-        scale: g.size.x / g.scale.x,
-        label: {
-          anchorX: "middle",
-          anchorY: "top",
-          offset: [0, -4],
-          visible: g.visible,
-          fontSize: 14
-        },
-        visible: g.visible,
-        minorTicks: 3
-      });
-
-      bR.create("ticks", [g.lines.top, g.delta.x], {
-        anchor: g.anchor.x,
-        includeBoundaries: true,
-        drawLabels: false,
-        fixed: true,
-        scale: g.size.x / g.scale.x,
-        visible: g.visible,
-        minorTicks: 3
-      });
-
-      bR.create("ticks", [g.lines.left, g.delta.y], {
-        anchor: g.anchor.y,
-        includeBoundaries: true,
-        drawLabels: true,
-        drawZero: true,
-        fixed: true,
-        scale: g.size.y / g.scale.y,
-        label: {
-          anchorX: "right",
-          anchorY: "middle",
-          offset: [-6, 0],
-          visible: g.visible,
-          fontSize: 14
-        },
-        visible: g.visible,
-        minorTicks: 3
-      });
-
-      bR.create("ticks", [g.lines.right, g.delta.y], {
-        anchor: g.anchor.y,
-        includeBoundaries: true,
-        drawLabels: false,
-        fixed: true,
-        scale: g.size.y / g.scale.y,
-        visible: g.visible,
-        minorTicks: 3
-      });
-
-      if (g.func != undefined) {
-        // curve specific options
-      }
-
-      // Text
-      if (g.title != "") {
-        this.textToUpdate[key + "_1"] = {};
-        this.textToUpdate[key + "_1"].object = bR.create("text", [g.corner.x + 0.5 * g.size.x, g.corner.y + g.size.y + 0.5, ""], {
-          fixed: true,
-          anchorX: "middle",
-          anchorY: "bottom",
-          visible: g.visible
+      } else {
+        bR.create("line", [points[data[0]], points[data[1]]], {
+          visible: valCheck("comps", "on"),
+          ...lineSegProps,
+          strokeColor: "red",
+          strokeWidth: 3,
+          lastArrow: true
         });
-        this.textToUpdate[key + "_1"].formula = "<b>" + g.title + "</b>";
-      }
-
-      if (g.axis.x != "") {
-        this.textToUpdate[key + "_2"] = {};
-        this.textToUpdate[key + "_2"].object = bR.create("text", [g.corner.x + 0.5 * g.size.x, g.corner.y - 0.5, ""], {
-          fixed: true,
-          anchorX: "middle",
-          anchorY: "top",
-          visible: g.visible
-        });
-        this.textToUpdate[key + "_2"].formula = g.axis.x;
-      }
-
-      if (g.axis.y != "") {
-        this.textToUpdate[key + "_3"] = {};
-        this.textToUpdate[key + "_3"].object = bR.create("text", [g.corner.x - 1.25, g.corner.y + 0.5 * g.size.y, ""], {
-          fixed: true,
-          anchorX: "middle",
-          anchorY: "bottom",
-          rotate: 90,
-          display: "internal",
-          visible: g.visible
-        });
-        this.textToUpdate[key + "_3"].formula = g.axis.y;
       }
     }
 
-    const hidden = { fixed: true, visible: false };
-    const hiddenLabel = { fixed: true, visible: true, withLabel: true, size: 0 };
+    for (let data of [
+      [1 / 2, 0, 0, "x_B - x_A", [0, -5]],
+      [1, 1 / 2, 0, "y_B - y_A", [5, 0]],
+      [1, 1, 1 / 2, "z_B - z_A", [5, 0]]
+    ]) {
+      bR.create(
+        "point",
+        [
+          () => {
+            return comp.x(
+              sliders.xA.Value() + data[0] * comp.xDiff(),
+              sliders.yA.Value() + data[1] * comp.yDiff(),
+              sliders.zA.Value() + data[2] * comp.zDiff()
+            );
+          },
+          () => {
+            return comp.y(
+              sliders.xA.Value() + data[0] * comp.xDiff(),
+              sliders.yA.Value() + data[1] * comp.yDiff(),
+              sliders.zA.Value() + data[2] * comp.zDiff()
+            );
+          }
+        ],
+        { ...hiddenLabelProps, name: data[3], label: { fontSize: 14, strokeColor: "red", visible: valCheck("comps", "on"), offset: data[4] } }
+      );
+    }
+
+    points.a = bR.create("point", [points["0"], COPY], {
+      visible: valCheck("pointA", "on"),
+      name: "<b>A</b>",
+      strokeColor: "blue",
+      fillColor: "blue",
+      label: { strokeColor: "blue", fontSize: 16 }
+    });
+    points.b = bR.create("point", [points["7"], COPY], {
+      visible: valCheck("pointB", "on"),
+      name: "<b>B</b>",
+      strokeColor: "green",
+      fillColor: "green",
+      label: { strokeColor: "green", fontSize: 16 }
+    });
+
+    bR.create("line", [points.a, points.b], {
+      visible: valCheck("pos_vec", "on"),
+      ...lineSegProps,
+      strokeColor: "red",
+      strokeWidth: 5,
+      lastArrow: true
+    });
 
     bL.addChild(bR);
     bR.addChild(bL);

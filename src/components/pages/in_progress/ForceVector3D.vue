3363746bf10cc1c3d9c37d71d8a3229842114b27
Jason Watkins <jmw4dx@virginia.edu>
2020-08-08 23:56:05 -0400

    More text

--- src/components/pages/in_progress/ForceVector3D.vue
+++ src/components/pages/in_progress/ForceVector3D.vue
@@ -2,11 +2,44 @@
   <div class="container-fluid">
     <h1 class="text-danger text-center my-4">Force Vector 3D</h1>
     <p>
-      Upper text
+      For the 3D case, a force vector can be represented as
     </p>
     <div class="text-center">
-      <p>Centered text</p>
+      <p>
+        <vue-mathjax
+          formula="$\vec{F} = F_x \vec{i} + F_y \vec{j} + F_z \vec{k} = \sqrt{F_x^2 + F_y^2 + F_z^2} 
+      \left( \frac{F_x}{\sqrt{F_x^2 + F_y^2 + F_z^2}} \vec{i} + \frac{F_y}{\sqrt{F_x^2 + F_y^2 + F_z^2}} \vec{j} + \frac{F_z}{\sqrt{F_x^2 + F_y^2 + F_z^2}} \vec{k}  \right)
+      = || \vec{F} || \left( \frac{\vec{F_x}}{|| \vec{F} ||} \vec{i} + \frac{\vec{F_y}}{|| \vec{F} ||} \vec{j} + \frac{\vec{F_z}}{|| \vec{F} ||} \vec{k} \right)$"
+        />
+      </p>
     </div>
+    <p>
+      Note that <vue-mathjax formula="$\cos \alpha = \frac{F_x}{||\vec{F}||}$" />, <vue-mathjax formula="$\cos \beta = \frac{F_y}{||\vec{F}||}$" />,
+      <vue-mathjax formula="$\cos \gamma = \frac{F_z}{||\vec{F}||}$" />, where <vue-mathjax formula="$\alpha$" /> is the angle between the vector and
+      x-axis, <vue-mathjax formula="$\beta$" /> is the angle between the vector and y-axis, <vue-mathjax formula="$\gamma$" /> is the angle between
+      the vector and z-axis, they satisfy the relations <vue-mathjax formula="$\cos^2 \alpha + \cos^2 \beta + \cos^2 \gamma = 1$" />. Then we have
+    </p>
+    <div class="text-center">
+      <p>
+        <vue-mathjax
+          formula="$\vec{F} = ||\vec{F}|| \left( \cos \alpha \ \vec{i} + \cos \beta \ \vec{j} + \cos \gamma \ \vec{k} \right) = ||\vec{F}||\vec{u}$"
+        />
+      </p>
+    </div>
+    <p>
+      Where <vue-mathjax formula="$\vec{u} = \left( \cos \alpha \ \vec{i} + \cos \beta \ \vec{j} + \cos \gamma \ \vec{k} \right)$" /> is the unit
+      vector.
+    </p>
+    <p>By changing magnitudes, we can get different <vue-mathjax formula="$\vec{F}$" />.</p>
+    <p>Alternatively, we can use the components to control <vue-mathjax formula="$\vec{F}$" />. Since we have</p>
+    <div class="text-center">
+      <p>
+        <vue-mathjax formula="$\vec{F} = F_x \vec{i} + F_y \vec{j} + F_z \vec{k}$" />
+      </p>
+    </div>
+    <p>
+      By changing the components directly <vue-mathjax formula="$F_x, F_y, F_z$" /> direcly, we can get different <vue-mathjax formula="$\vec{F}$" />.
+    </p>
     <fv1 />
   </div>
 </template>
--- src/components/pages/in_progress/PosVector3D.vue
+++ src/components/pages/in_progress/PosVector3D.vue
@@ -2,10 +2,22 @@
   <div class="container-fluid">
     <h1 class="text-danger text-center my-4">Position Vector 3D</h1>
     <p>
-      Upper text
+      A position vector <vue-mathjax formula="$\vec{r}$" /> is defined as a fixed vector which locates a point in space relative to another point.
+    </p>
+    <p>
+      If we know the coordinates of a point, say A, as <vue-mathjax formula="${x_A, y_A, z_A}$" />, then the position vector from the origin point to
+      A will be
     </p>
     <div class="text-center">
-      <p>Centered text</p>
+      <p><vue-mathjax formula="$\vec{r_A} = x_A \vec{i} + y_A \vec{j} + z_A \vec{k}$" /></p>
+    </div>
+    <p>Similarly, for point B with coordinates <vue-mathjax formula="${x_B, y_B, z_B}$" />, the position vector from the origin point to B will be</p>
+    <div class="text-center">
+      <p><vue-mathjax formula="$\vec{r_B} = x_B \vec{i} + y_B \vec{j} + z_B \vec{k}$" /></p>
+    </div>
+    <p>Thus, the position vector from point A to point B will be</p>
+    <div class="text-center">
+      <p><vue-mathjax formula="$\vec{r} = \vec{r_B} - \vec{r_A} = (x_B-x_A) \vec{i} + (y_B-y_A) \vec{j} + (z_B - z_A) \vec{k}$" /></p>
     </div>
     <pv />
   </div>

2cb4873c02594876dfdaff9e8e6212b0e43dd614
Jason Watkins <jmw4dx@virginia.edu>
2020-09-12 21:23:09 -0400

    New module 9

---
+++ src/components/pages/in_progress/MomentForce2D.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-fluid">
+    <h1 class="text-danger text-center my-4">Moment of a Force 2D</h1>
+    <p>
+      The moment of a force Overscript[F, \[RightVector]] about point O, or actually about the moment axis passing through O and perpendicular to the
+      plane containing O and Overscript[F, \[RightVector]], can be expressed using the vector cross product, namely Subscript[Overscript[M,
+      \[RightVector]], O]=Overscript[r, \[RightVector]]*Overscript[F, \[RightVector]]. Here Overscript[r, \[RightVector]] represents a position vector
+      directed from O to any point on the line of action of Overscript[F, \[RightVector]]. For a beam with unbalanced moment Subscript[Overscript[M,
+      \[RightVector]], O], the equation of motion is described by Subscript[Overscript[M, \[RightVector]], O]= Subscript[I,
+      O]Overscript[Overscript[\[Theta], ¨], \[RightVector]], where Overscript[Overscript[\[Theta], ¨], \[RightVector]] is the angular acceleration,
+      Subscript[I, O] is the moment of inertia with respect to O. If the beam is constrained to be rotate within certain plane, the equation of motion
+      is simplified as Subscript[M, O]= Subscript[I, O]Overscript[\[Theta], ¨]. So we see, in order to keep the beam in static, we need to make the
+      Subscript[M, O] to zero. For the following problem, we have two forces Subscript[Overscript[F, \[RightVector]], 1],Subscript[Overscript[F,
+      \[RightVector]], 2], with their magnitudes, directions and acting positions to be set. Both of them produce the moment with respect to hinge,
+      and the resultant may cause the moment to rotate with respect to hinge. We can see the rotation once we set unbalanced moment of these two
+      forces. Also, we can see that the beam will keep in static once the resultant moment is zero.
+    </p>
+
+    <!-- <div class="text-center">
+      <p><vue-mathjax formula="$\vec{r}$" /></p>
+    </div> -->
+    <mf />
+  </div>
+</template>
+<script>
+import moment_of_force from "../../utils/MomentOfForce/moment_of_force_2D.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    mf: moment_of_force
+  }
+};
+export const meta = {
+  title: "Moment of Force 2D",
+  description: "",
+  number: 9
+};
+</script>
+
+<style>
+p {
+  font-size: 16;
+}
+</style>
---
+++ src/components/utils/MomentOfForce/moment_of_force_2D.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-3 justify-content-center">
+      <div id="boxLeft" class="boxLeft my-2" style="width:425px; height:700px;"></div>
+
+      <div id="boxRight" class="boxRight my-2" style="width:700px; height:700px;"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { InputHandler } from "../../../classes/InputHandler.js";
+export default {
+  components: {},
+  data: () => {
+    return {
+      IH: undefined,
+      bL: undefined,
+      bR: undefined
+    };
+  },
+  mounted() {
+    // Setup the two boards
+    // The lefthand board (sliders and text)
+    const bL = JXG.JSXGraph.initBoard("boxLeft", {
+      boundingbox: [-15, 15, 15, -15],
+      keepAspectRatio: true,
+      showCopyright: false,
+      pan: { enabled: false },
+      zoom: { wheel: false },
+      showNavigation: false,
+      showZoom: false,
+      axis: false
+    });
+    // The size of the container (matches explicit css style in html)
+    bL.resizeContainer(425, 700);
+
+    const bR = JXG.JSXGraph.initBoard("boxRight", {
+      boundingbox: [-15, 15, 15, -15],
+      keepAspectRatio: true,
+      showCopyright: false,
+      pan: { enabled: false },
+      zoom: { wheel: false },
+      showNavigation: false,
+      showZoom: false,
+      axis: false
+    });
+    // The size of the container (matches explicit css style in html)
+    bR.resizeContainer(700, 700);
+
+    // All sliders are stored in these objects
+    // They can be accessed with brackets sliders["force"] or dot notation sliders.force
+    this.bL = bL;
+    this.bR = bR;
+
+    let sliders = {};
+    const LABEL_SIZE = 18;
+
+    const INTERVAL = -4.5;
+    // Generate sliders, along with their related components
+    // Can either have textbox + update button, or on/off system
+    const TOP_Y = 12;
+    const LEFT_X = -1;
+    const RIGHT_X = 5;
+
+    const INITIAL_VALUES = { coords: "off" };
+    let IH = new InputHandler(bL, INTERVAL, TOP_Y, LEFT_X, RIGHT_X, LABEL_SIZE, INITIAL_VALUES, 0);
+    this.IH = IH;
+
+    for (let data of [
+      /* [key value,
+          label name,
+          y position,
+          true: update button | false: on/off,
+          [min, start, max] (if true) | [lbl1, val1, lbl2, val2] (if false),
+          [allowed states],
+          color ]*/
+      /* For just text: [false, label name, mathJax] */
+      ["coords", "Coordinate Visibility", INTERVAL * 0, false, ["On", "on", "Off", "off"], [0]],
+      ["h_pos", "Hinge Position (m)", INTERVAL * 0.5, true, [0, 0, 3], [0], "black"],
+      ["f1_mag", "F_1 Magnitude (N)", INTERVAL * 1.5, true, [-3, 0, 3], [0], "black"],
+      ["f1_angle", "Angle of F_1: \u03b1_1 (\u00b0)", INTERVAL * 2.5, true, [0, 0, 360], [0], "black"],
+      ["f1_pos", "F_1 Position (m)", INTERVAL * 3.5, true, [0, 0, 3], [0], "black"],
+      ["f2_mag", "F_2 Magnitude (N)", INTERVAL * 4.5, true, [-3, 0, 3], [0], "black"],
+      ["f2_angle", "Angle of F_2: \u03b1_2 (\u00b0)", INTERVAL * 5.5, true, [0, 0, 360], [0], "black"],
+      ["f2_pos", "F_2 Position (m)", INTERVAL * 6.5, true, [0, 0, 3], [0], "black"]
+    ]) {
+      IH.generate(data, sliders);
+    }
+
+    let convX = (x, g) => {
+      let zero = g.corner.x + g.size.x * g.anchor.x;
+      return zero + (x / g.scale.x) * g.size.x;
+    };
+
+    let convY = (y, g) => {
+      let zero = g.corner.y + g.size.y * g.anchor.y;
+      return zero + (y / g.scale.y) * g.size.y;
+    };
+
+    let convXY = (x, y, g) => {
+      return [convX(x, g), convY(y, g)];
+    };
+
+    // Handles all calculations
+    let comp = {};
+    comp.zero = () => {
+      return 0;
+    };
+    comp.radians = deg => {
+      return (deg * Math.PI) / 180;
+    };
+    comp.degrees = rad => {
+      return (rad * 180) / Math.PI;
+    };
+    // comp.magnitude = v => {
+    //   // Assumes 3D vector as input
+    //   return Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    // };
+    // comp.scalar_mult = (v, s) => {
+    //   // Assumes 3D vector as input
+    //   return [v[0] * s, v[1] * s, v[2] * s];
+    // };
+    // comp.dot_mult = (v1, v2) => {
+    //   return v2[0] * v1[0] + v2[1] * v1[1] + v2[2] * v1[2];
+    // };
+    // comp.cross_prod = (u, v) => {
+    //   let u1 = u[0],
+    //     u2 = u[1],
+    //     u3 = u[2],
+    //     v1 = v[0],
+    //     v2 = v[1],
+    //     v3 = v[2];
+    //   return [u2 * v3 - v2 * u3, -(u1 * v3 - v1 * u3), u1 * v2 - v1 * u2];
+    // };
+    // comp.vector_add = (v2, v1) => {
+    //   return [v2[0] + v1[0], v2[1] + v1[1], v2[2] + v1[2]];
+    // };
+    // comp.vector_sub = (v2, v1) => {
+    //   return [v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]];
+    // };
+    // comp.unit_vector = v => {
+    //   // Assumes 3D vector as input
+    //   let mag = comp.magnitude(v);
+    //   if (mag != 0) return [v[0] / mag, v[1] / mag, v[2] / mag];
+    //   else return [0, 0, 0];
+    // };
+
+    let graphs = {
+      /*
+      arbitrary name of graph
+        corner: lower left point coordinates
+        size: length of graph on each side
+        delta: tick distancing
+        scale: length of relative space to exist on each axis
+        anchor: 0 - lowest point, 1 - highest point, 0.5 - middle, location of 0
+        axis: Axes labels
+        Title: title label
+        func: what the curve should use
+        Color: undefined for no curve, color for curve
+      */
+      large: {
+        corner: { x: -13, y: -13 },
+        size: { x: 26, y: 26 },
+        delta: { x: 1, y: 1 },
+        scale: { x: 3.25 * 2, y: 3.25 * 2 },
+        anchor: { x: 0.5, y: 0.5 },
+        axis: { x: "", y: "" },
+        title: "",
+        visible: true,
+        func: undefined,
+        color: "blue",
+        outlines: false
+      }
+    };
+
+    // Setup the graphs
+    for (let key of Object.keys(graphs)) {
+      // The graph of focus
+      let g = graphs[key];
+
+      // Generate the lines
+      g.lines = {};
+      for (let data of [
+        ["left", [g.corner.x, g.corner.y], [g.corner.x, g.corner.y + g.size.y]],
+        ["top", [g.corner.x, g.corner.y + g.size.y], [g.corner.x + g.size.x, g.corner.y + g.size.y]],
+        ["right", [g.corner.x + g.size.x, g.corner.y], [g.corner.x + g.size.x, g.corner.y + g.size.y]],
+        ["bottom", [g.corner.x, g.corner.y], [g.corner.x + g.size.x, g.corner.y]]
+      ]) {
+        g.lines[data[0]] = bR.create("line", [data[1], data[2]], {
+          straightFirst: false,
+          straightLast: false,
+          fixed: true,
+          strokeWidth: 3,
+          strokeColor: "black",
+          visible: g.visible
+        });
+      }
+
+      // Generate the ticks
+      bR.create("ticks", [g.lines.bottom, g.delta.x], {
+        anchor: g.anchor.x,
+        includeBoundaries: true,
+        drawLabels: true,
+        drawZero: true,
+        fixed: true,
+        scale: g.size.x / g.scale.x,
+        label: {
+          anchorX: "middle",
+          anchorY: "top",
+          offset: [0, -4],
+          visible: g.visible,
+          fontSize: 14
+        },
+        visible: g.visible,
+        minorTicks: 3
+      });
+
+      bR.create("ticks", [g.lines.top, g.delta.x], {
+        anchor: g.anchor.x,
+        includeBoundaries: true,
+        drawLabels: false,
+        fixed: true,
+        scale: g.size.x / g.scale.x,
+        visible: g.visible,
+        minorTicks: 3
+      });
+
+      bR.create("ticks", [g.lines.left, g.delta.y], {
+        anchor: g.anchor.y,
+        includeBoundaries: true,
+        drawLabels: true,
+        drawZero: true,
+        fixed: true,
+        scale: g.size.y / g.scale.y,
+        label: {
+          anchorX: "right",
+          anchorY: "middle",
+          offset: [-6, 0],
+          visible: g.visible,
+          fontSize: 14
+        },
+        visible: g.visible,
+        minorTicks: 3
+      });
+
+      bR.create("ticks", [g.lines.right, g.delta.y], {
+        anchor: g.anchor.y,
+        includeBoundaries: true,
+        drawLabels: false,
+        fixed: true,
+        scale: g.size.y / g.scale.y,
+        visible: g.visible,
+        minorTicks: 3
+      });
+
+      if (g.func != undefined) {
+        // curve specific options
+      }
+
+      // Text
+      if (g.title != "") {
+        IH.textToUpdate[key + "_1"] = {};
+        IH.textToUpdate[key + "_1"].object = bR.create("text", [g.corner.x + 0.5 * g.size.x, g.corner.y + g.size.y + 0.5, ""], {
+          fixed: true,
+          anchorX: "middle",
+          anchorY: "bottom",
+          visible: g.visible
+        });
+        IH.textToUpdate[key + "_1"].formula = "<b>" + g.title + "</b>";
+      }
+
+      if (g.axis.x != "") {
+        IH.textToUpdate[key + "_2"] = {};
+        IH.textToUpdate[key + "_2"].object = bR.create("text", [g.corner.x + 0.5 * g.size.x, g.corner.y - 0.5, ""], {
+          fixed: true,
+          anchorX: "middle",
+          anchorY: "top",
+          visible: g.visible
+        });
+        IH.textToUpdate[key + "_2"].formula = g.axis.x;
+      }
+
+      if (g.axis.y != "") {
+        IH.textToUpdate[key + "_3"] = {};
+        IH.textToUpdate[key + "_3"].object = bR.create("text", [g.corner.x - 1.25, g.corner.y + 0.5 * g.size.y, ""], {
+          fixed: true,
+          anchorX: "middle",
+          anchorY: "bottom",
+          rotate: 90,
+          display: "internal",
+          visible: g.visible
+        });
+        IH.textToUpdate[key + "_3"].formula = g.axis.y;
+      }
+    }
+
+    const hidden = { fixed: true, visible: false };
+    const hiddenLabel = { fixed: true, visible: true, withLabel: true, size: 0 };
+
+    bL.addChild(bR);
+    bR.addChild(bL);
+    this.changeState(0);
+  },
+  methods: {
+    changeState(newState) {
+      this.IH.updateState(newState);
+    }
+  }
+};
+</script>
+
+<style>
+button:disabled {
+  opacity: 0.5;
+  /* visibility: hidden; */
+}
+input:disabled {
+  opacity: 0.7;
+  /* visibility: hidden; */
+}
+</style>
